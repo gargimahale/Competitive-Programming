58e90cfc41d1ab650e41ee289053e1f277b39180
gargimahale <gargimahale102@gmail.com>
2021-02-20 01:20:43 -0500

    Added LeetCode files

---
+++ LeetCode/Easy/Kids With the Greatest Number of Candies.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxNum = 0;
+        for (int x: candies){
+            maxNum = max(maxNum, x);
+        }
+        
+        vector<bool> res(candies.size(), false);
+        for (int i=0; i<candies.size(); ++i){
+            if (candies[i] + extraCandies >= maxNum){
+                res[i] = true;
+            }
+        }
+        return res;
+    }
+};
