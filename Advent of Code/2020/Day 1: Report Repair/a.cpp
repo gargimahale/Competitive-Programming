7c1ff4beef71308d4021f83f66049aa293271024
gargimahale <gargimahale102@gmail.com>
2021-02-21 19:16:20 -0500

    Added Advent of Code Day 1 files

---
+++ Advent of Code/2020/Day 1: Report Repair/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 2-Sum
+
+int main(){
+    ifstream myfile("inp.txt");
+    unordered_map<int, bool> U;
+    int target = 2020;
+    int input;
+    while(myfile >> input){
+        if (U.find(target-input) != U.end()){
+            cout << input << " " << target-input << "\n";
+            break;
+        }else{
+            U[input] = 1;
+        }
+    }
+    return 0;
+}
---
+++ Advent of Code/2020/Day 1: Report Repair/b.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 3-Sum
+
+void threeSum(vector<int>& nums, int target){
+    int ans = 1;
+    sort(begin(nums), end(nums));
+    for (int i=0; i<nums.size()-2; ++i){
+        if (i == 0 || nums[i] != nums[i-1]){
+            int j = i+1, k = nums.size()-1;
+            while(j < k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if (sum == target){
+                    ans = nums[i]*nums[j]*nums[k];
+                    cout << ans << "\n";
+                    return;
+                }else if (sum > target){
+                    --k;
+                }else{
+                    ++j;
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    ifstream myfile("inp.txt");
+    int input, target = 2020;
+    vector<int> nums;
+    while(myfile >> input){
+        nums.push_back(input);
+    }
+    threeSum(nums, target);
+    return 0;
+}
