0fe2072c87b8caa22e9fedb6fa3e8eee2f85651b
gargimahale <gargimahale102@gmail.com>
2021-02-27 21:45:44 -0500

    Added LeetCode Daily files

---
+++ LeetCode/Easy/Count Items Matching a Rule.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        unordered_map<string, int> type, color, name;
+        for (auto x: items){
+            type[x[0]]++;
+            color[x[1]]++;
+            name[x[2]]++;
+        }
+        
+        if (ruleKey == "type"){
+            return type[ruleValue];
+        }
+        else if (ruleKey == "color"){
+            return color[ruleValue];
+        }
+        return name[ruleValue];
+    }
+};
