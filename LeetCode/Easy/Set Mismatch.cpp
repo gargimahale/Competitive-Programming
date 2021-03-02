a58b36d1ae52b94265beb401dfcacf6b521f1ada
gargimahale <gargimahale102@gmail.com>
2021-03-02 15:32:45 -0500

    Added LeetCode Daily files

---
+++ LeetCode/Easy/Set Mismatch.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> ans(2);
+        int sum = 0, n = nums.size(), i;
+        vector<int> count(n+1);
+        for (i=0; i<n; ++i){
+            sum += nums[i];
+            count[nums[i]]++;
+            if (count[nums[i]] > 1){
+                ans[0] = nums[i];
+                break;
+            }
+        }
+
+        // i++ is done initailly to skip that duplicate number
+        for(i++; i<n; ++i){
+            sum += nums[i];
+        }
+        
+        ans[1] = ((n*(n+1)/2) - sum + ans[0]);
+        return ans;
+    }
+};
