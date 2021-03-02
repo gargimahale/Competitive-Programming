3298a4b82cb617c2290a47301d195f839595367b
gargimahale <gargimahale102@gmail.com>
2021-03-01 20:50:47 -0500

    Added LeetCode Daily files

---
+++ LeetCode/Easy/Single-Row Keyboard.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int calculateTime(string keyboard, string word) {
+        vector<int> check(26);
+        for (int i=0; i<keyboard.size(); ++i){
+            check[keyboard[i]-'a'] = i;
+        }
+        
+        int ans = check[word[0]-'a'];
+        for(int i=1; i<word.size(); ++i){
+            ans += (abs(check[word[i-1]-'a'] - check[word[i]-'a']));
+        }
+        return ans;
+    }
+};
