2305a73f22aeafdcfa4c4242b00d1af06b86851c
gargimahale <gargimahale102@gmail.com>
2021-03-03 18:04:40 -0500

    Added LeetCode Daily files

---
+++ LeetCode/Easy/Invert Binary Tree.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+
+        // Recursive Solution
+        // if (root){
+        //     invertTree(root->left);
+        //     invertTree(root->right);
+        //     swap(root->left, root->right);
+        // }
+        // return root;
+        
+        // Iterative Solutions
+        stack<TreeNode*> stk;
+        stk.push(root);
+        while(!stk.empty()){
+            TreeNode* p = stk.top(); stk.pop();
+            if (p){
+                stk.push(p->left);
+                stk.push(p->right);
+                swap(p->left, p->right);
+            }
+        }
+        return root;
+    }
+};
