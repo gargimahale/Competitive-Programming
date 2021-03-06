bde2411b41ec141dead7865208dd56b8d1f0bc00
gargimahale <gargimahale102@gmail.com>
2021-03-06 01:53:20 -0500

    Added LeetCode Files

---
+++ LeetCode/Hard/Serialize and Deserialize Binary Tree.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if (!root) return "#";
+        return to_string(root->val) + "," + serialize(root->left) + "," + serialize(root->right);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if (data == "#") return NULL;
+        stringstream ss(data);
+        return helperDeserialize(ss);
+    }
+    
+    TreeNode* helperDeserialize(stringstream& ss){
+        string str;
+        getline(ss, str, ',');
+        if (str == "#") return NULL;
+        else{
+            TreeNode* root = new TreeNode(stoi(str));
+            root->left = helperDeserialize(ss);
+            root->right = helperDeserialize(ss);
+            return root;
+        }
+    }
+    
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
