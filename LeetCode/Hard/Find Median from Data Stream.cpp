482be3aa4c94e56e100d96b6cfe386d2ed3b0a4a
gargimahale <gargimahale102@gmail.com>
2021-03-06 01:32:23 -0500

    Added LeetCode Files

---
+++ LeetCode/Hard/Find Median from Data Stream.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MedianFinder {
+public:
+    /** initialize your data structure here. */
+    priority_queue<int> low, high;
+    
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        low.push(num);
+        high.push(-low.top());
+        low.pop();
+        if (low.size() < high.size()){
+            low.push(-high.top());
+            high.pop();
+        }
+    }
+    
+    double findMedian() {
+        return low.size() > high.size() ? low.top() : (low.top()-high.top()) * 0.5;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
+
