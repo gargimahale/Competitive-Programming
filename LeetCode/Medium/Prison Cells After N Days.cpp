8312a67cf75cada622f6ba914fddaf3329ac9fd0
gargimahale <gargimahale102@gmail.com>
2021-03-07 00:38:07 -0500

    Added LeetCode Files

---
+++ LeetCode/Medium/Prison Cells After N Days.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> prisonAfterNDays(vector<int>& cells, int N) {
+        int n = cells.size();
+        vector<int> res(n), v;
+        int cycle = 0;
+        while(N-- > 0){
+            for (int i=1; i<n-1; ++i){
+                res[i] = cells[i-1] == cells[i+1];
+            }
+            
+            if (v.empty()){
+                v = res;
+            }else if (v == res){
+                N%=cycle;
+            }
+            
+            ++cycle;
+            cells = res;
+        }
+        return cells;
+    }
+};
