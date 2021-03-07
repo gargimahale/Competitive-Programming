b2d4e34cc1883bcf5fc78a6ef489f664a9197b05
gargimahale <gargimahale102@gmail.com>
2021-03-07 16:20:01 -0500

    Added LeetCode Files

--- LeetCode/Medium/copy_list_random_pointer.cpp
+++ LeetCode/Medium/copy_list_random_pointer.cpp
@@ -17,42 +17,56 @@ public:
     }
 };
 
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if (!head){
-            return NULL;
-        }
-
-        Node* curr = head, *next;
-
+        
+        if (!head) return NULL;
+        
+        Node* curr = head, *dummy;
+        
         // create duplicate nodes
         while(curr){
-            next = curr->next;
+            dummy = curr->next;
             curr->next = new Node(curr->val);
-            curr->next->next = next;
-            curr = next;
+            curr->next->next = dummy;
+            curr = dummy;
         }
-
-        // setting the random pointers
+        
+        // assign random ptrs
         curr = head;
         while(curr){
-            if ( curr->random){
+            if (curr->random){
                 curr->next->random = curr->random->next;
             }
             curr = curr->next->next;
         }
-
-        // Revert
+        
+        // display list
         curr = head;
         Node* res = head->next;
         while(curr->next && curr->next->next){
-            next = curr->next;
+            dummy = curr->next;
             curr->next = curr->next->next;
-            next->next = curr->next->next;
+            dummy->next = curr->next->next;
             curr = curr->next;
         }
-
         curr->next = NULL;
         return res;
     }
