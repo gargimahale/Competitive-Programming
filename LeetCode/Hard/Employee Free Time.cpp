3ce208568c1393b8dcd1d67a54069b1fd6bcec78
gargimahale <gargimahale102@gmail.com>
2021-03-07 15:58:16 -0500

    Added LeetCode Files

--- LeetCode/Hard/Employee Free Time.cpp
+++ LeetCode/Hard/Employee Free Time.cpp
@@ -21,20 +21,23 @@ class Solution {
 public:
     vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
         vector<Interval> vec;
-        if(schedule.empty() || schedule[0].empty()) return vec;
-        for(auto &a:schedule) vec.insert(vec.end(), a.begin(),a.end());
-        sort(vec.begin(),vec.end(),[](auto &a, auto &b){return a.start<b.start ||(a.start == b.start && a.end<b.end);});
+        for (auto& s : schedule) vec.insert(vec.end(), s.begin(), s.end());
+        
+        sort(begin(vec), end(vec), [&](auto a, auto b){
+            return a.start < b.start || (a.start == b.start && a.end < b.end);
+        });
+        
         vector<Interval> res;
         int end = vec[0].end;
-        for(int i =1 ; i < vec.size(); ++i){
-            
-            if(vec[i].start <= end)
-                end = max(end,vec[i].end);
+        
+        for (int i=1; i<vec.size(); ++i){
+            if (vec[i].start <= end){
+                end = max(vec[i].end, end);
+            }
             else{
-                res.push_back(Interval(end,vec[i].start));
+                res.push_back(Interval(end, vec[i].start));
                 end = vec[i].end;
             }
-            
         }
         return res;
     }
