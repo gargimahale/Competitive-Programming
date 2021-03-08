3d62f8f9a5697c7366e1c6326a55a9987a3ba037
gargimahale <gargimahale102@gmail.com>
2021-03-08 16:44:31 -0500

    Added LeetCode Files

---
+++ LeetCode/Easy/Rectangle Overlap.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        //check for invalid test cases
+        if(rec2[0] == rec2[2] || rec1[1] == rec1[3] || rec2[1] == rec2[3] || rec1[0] == rec1[2]){
+            return false;
+        }
+        //check for don't overlap condition
+        if(rec1[2] <= rec2[0] || rec1[1] >= rec2[3] || rec1[0] >= rec2[2] || rec2[1] >= rec1[3]){
+            return false;
+        }
+        //otherwise overlap
+        return true;
+    }
+};
