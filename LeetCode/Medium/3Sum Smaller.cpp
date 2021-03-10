35df35d246532b771d2f2b772bb07c7638230adb
gargimahale <gargimahale102@gmail.com>
2021-03-10 16:36:07 -0500

    Added LeetCode Files

--- LeetCode/Medium/3Sum Smaller.cpp
+++ LeetCode/Medium/3Sum Smaller.cpp
@@ -12,7 +12,7 @@ public:
         sort(begin(nums), end(nums));
         for (int i=0; i<nums.size()-2; ++i){
             int j = i+1, k = nums.size()-1;
-            while(j<k){
+            while(j < k){
                 if (nums[i]+nums[j]+nums[k] < target){
                     count+= (k - j++);
                 }
