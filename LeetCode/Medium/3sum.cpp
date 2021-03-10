0cd38ca7ac2d1768a1caf2d29c1294db550108c7
gargimahale <gargimahale102@gmail.com>
2021-03-10 15:39:34 -0500

    Added LeetCode Files

--- LeetCode/Medium/3sum.cpp
+++ LeetCode/Medium/3sum.cpp
@@ -5,33 +5,35 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-        if (nums.size() < 3){
-            return {};
-        }
-
+        int n = nums.size();
+        vector<vector<int>> ans;
+        
         sort(nums.begin(), nums.end());
-
+        
+        // base case
+        if (n < 3) return ans;
+        
+        
         for (int i=0; i<nums.size()-2; ++i){
+            
+            // ignores duplicates
             if (i == 0 || nums[i] != nums[i-1]){
                 int j = i+1, k = nums.size()-1;
-                while(j<k){
+                while(j < k){
                     int sum = nums[i]+nums[j]+nums[k];
                     if (sum == 0){
-                        res.push_back({nums[i], nums[j], nums[k]});
-                        while(j < k && nums[j] == nums[j+1]) j++;
-                        while(j < 0 && nums[k] == nums[k-1]) k--;
-                        j++, k--;
+                        ans.push_back({nums[i], nums[j], nums[k]});
+                        
+                        // ignores dupliactes
+                        while(j < k && nums[j] == nums[j+1]) ++j;
+                        while(j < 0 && nums[k] == nums[k-1]) --k;
+                        ++j, --k;
                     }
-                    else if (sum > 0){
-                        k--;
-                    }
-                    else{
-                        j++;
-                    }
+                    else if (sum > 0) --k;
+                    else ++j;
                 }
             }
         }
-        return res;
+        return ans;
     }
 };
