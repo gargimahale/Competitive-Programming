d1d0dbcd96e4687b67039e373c94aac246635a91
gargimahale <gargimahale102@gmail.com>
2021-03-11 23:16:02 -0500

    Added LeetCode Files

--- LeetCode/Medium/ Find Minimum in Rotated Sorted Array.cpp
+++ LeetCode/Medium/ Find Minimum in Rotated Sorted Array.cpp
@@ -1,15 +1,13 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int l =0, h=nums.size()-1;
-        while(l<h){
+        int l = 0, h = nums.size()-1;
+        while(l < h){
             int mid = l+(h-l)/2;
-            if(nums[mid] > nums[h]) l=mid+1;
-            else h=mid;
+            if(nums[mid] > nums[h]) l = mid+1;
+            else h = mid;
         }
         return nums[l];
     }
---
+++ LeetCode/Medium/Coin Change.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amt) {
+        int n = coins.size();
+        vector<int> dp(amt+1, amt+1);
+        dp[0] = 0;
+        for (int i=1; i<=amt; ++i){
+            for(int j = 0; j<n; ++j){
+                if (coins[j] <= i){
+                    dp[i] = min(dp[i], dp[i-coins[j]]+1);
+                }
+            }
+        }
+        return dp[amt] > amt ? -1: dp[amt]; 
+    }
+};
