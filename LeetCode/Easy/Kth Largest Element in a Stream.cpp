95cc259c7129ed09ca19868ed660ce8e7f1c2f01
gargimahale <gargimahale102@gmail.com>
2021-03-18 20:06:39 -0400

    Added LeetCode Files

---
+++ LeetCode/Easy/Kth Largest Element in a Stream.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class KthLargest
+{
+public:
+    int K;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    KthLargest(int k, vector<int> &nums)
+    {
+        K = k;
+        for (int x : nums)
+            add(x);
+    }
+
+    int add(int val)
+    {
+        if (pq.size() < K || pq.top() < val)
+            pq.push(val);
+        if (pq.size() > K)
+            pq.pop();
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
