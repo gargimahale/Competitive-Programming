152bb4cb7147a9004368f24eec5e830114664f31
gargimahale <gargimahale102@gmail.com>
2021-03-19 01:17:42 -0400

    Added LeetCode Files

--- LeetCode/Medium/K Closest Points to Origin.cpp
+++ LeetCode/Medium/K Closest Points to Origin.cpp
@@ -1,3 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution{
+public:
+    vector<vector<int>> kClosest(vector<vector<int>> &points, int K){
+        struct mycomp{
+            bool operator()(const pair<double, int> &a, const pair<double, int> &b){
+                return a.first > b.first;
+            }
+        };
+        // distance, index
+        priority_queue<pair<double, int>, vector<pair<double, int>>, mycomp> m;
+
+        for (int i = 0; i < points.size(); i++){
+            double calc = sqrt(pow(points[i][0], 2) + pow(points[i][1], 2));
+            m.push(make_pair(calc, i));
+        }
+
+        vector<vector<int>> answer;
+        for (int i = 0; i < K; i++){
+            answer.push_back(points[m.top().second]);
+            m.pop();
+        }
+        return answer;
+    }
+};
