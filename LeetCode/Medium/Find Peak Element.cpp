baf88b2ae7061bedc48038b6c43dd13b50a26d1a
gargimahale <gargimahale102@gmail.com>
2021-03-23 17:48:02 -0400

    Added LeetCode Files

---
+++ LeetCode/Medium/Find Peak Element.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findPeakElement(vector<int> &nums)
+    {
+        int l = 0, h = nums.size() - 1;
+        while (l < h)
+        {
+            int m = l + (h - l) / 2;
+            if (nums[m] < nums[m + 1])
+                l = m + 1;
+            else
+                h = m;
+        }
+        return l;
+    }
+};
