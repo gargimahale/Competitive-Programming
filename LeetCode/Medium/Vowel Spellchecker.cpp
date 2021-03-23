08061f99798fdd41e4b53da6bba8e60a0c0a575b
gargimahale <gargimahale102@gmail.com>
2021-03-23 03:09:32 -0400

    Added LeetCode Files

---
+++ LeetCode/Medium/Vowel Spellchecker.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+
+	string tolow(string s) {
+		for (auto& ch : s) ch = tolower(ch);
+		return s;
+	}
+
+	string todev(string s) {
+		s = tolow(s);
+		for (auto& ch : s) {
+			if (ch == 'a' || ch == 'i' || ch == 'e' || ch == 'o' || ch == 'u') ch = '#';
+		}
+		return s;
+	}
+
+
+	vector<string> spellchecker(vector<string>& wordlist, vector<string>& queries) {
+		unordered_set<string> words(begin(wordlist), end(wordlist));
+		unordered_map<string, string> cap, vowel;
+		for (string w : wordlist) {
+			string lower = tolow(w), devowel = todev(w);
+			cap.insert({lower, w});
+			vowel.insert({devowel, w});
+		}
+		for (int i = 0; i < queries.size(); ++i) {
+			if (words.count(queries[i])) continue;
+			string lower = tolow(queries[i]), devowel = todev(queries[i]);
+			if (cap.count(lower)) {
+				queries[i] = cap[lower];
+			}
+			else if (vowel.count(devowel)) {
+				queries[i] = vowel[devowel];
+			}
+			else {
+				queries[i] = "";
+			}
+		}
+		return queries;
+	}
+};
