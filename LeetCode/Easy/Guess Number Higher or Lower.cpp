3e0989cb1980a9c0e323253334c83f4fc70a91d0
gargimahale <gargimahale102@gmail.com>
2021-03-23 16:33:50 -0400

    Added LeetCode Files

---
+++ LeetCode/Easy/Guess Number Higher or Lower.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution
+{
+public:
+    int guessNumber(int n)
+    {
+        int lo = 1, hi = n;
+        while (lo <= hi)
+        {
+            int m = lo + (hi - lo) / 2;
+            int ans = guess(m);
+            if (ans == 0)
+                return m;
+            else if (ans == -1)
+                hi = m - 1;
+            else
+                lo = m + 1;
+        }
+        return -1;
+    }
+};
