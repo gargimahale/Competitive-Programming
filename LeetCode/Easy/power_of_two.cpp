9f8900d2adab9c6bfaa78204e88c7f4cc15a26fb
gargimahale <gargimahale102@gmail.com>
2021-03-24 01:16:07 -0400

    Added LeetCode Files

--- LeetCode/Easy/power_of_two.cpp
+++ LeetCode/Easy/power_of_two.cpp
@@ -2,16 +2,9 @@
 
 using namespace std;
 
-bool isPowerofTwo(int n){
-    int cnt=0, temp=n;
-    while(temp>1) temp/=2, cnt++;
-    if(pow(2, cnt) == n) return true;
-    return false;
-}
-
-int main(){
-    int n;
-    cin >> n;
-    cout << isPowerofTwo(n);
-    return 0;
-}
+class Solution{
+public:
+    bool isPowerOfTwo(int n){
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+};
