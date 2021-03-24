6ff47a9bdd4d414213502e940d47d0a145cc2a2b
gargimahale <gargimahale102@gmail.com>
2021-03-23 20:02:55 -0400

    Added LeetCode Files

---
+++ LeetCode/Easy/Closest Binary Search Tree Value.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution{
+public:
+    int closestValue(TreeNode *root, double target){
+        int val, close = root->val;
+        while (root){
+            val = root->val;
+            close = abs(val - target) < abs(close - target) ? val : close;
+            root = target < root->val ? root->left : root->right;
+        }
+        return close;
+    }
+};
