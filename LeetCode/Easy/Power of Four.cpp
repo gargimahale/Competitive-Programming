55c605e9e7d55a832b8b54c3960740adb4ded7b7
gargimahale <gargimahale102@gmail.com>
2021-03-24 01:24:44 -0400

    Added LeetCode Files

---
+++ LeetCode/Easy/Power of Four.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    bool isPowerOfFour(int num){
+        return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0;
+    }
+};
