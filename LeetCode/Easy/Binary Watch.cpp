d8e71b8a4635f9797613979c171046b166d1f2dc
gargimahale <gargimahale102@gmail.com>
2021-03-24 16:33:54 -0400

    Added LeetCode Files

---
+++ LeetCode/Easy/Binary Watch.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int bit_count(int bits){
+        int cnt = 0;
+        for (; bits; bits &= bits - 1)
+            ++cnt;
+        return cnt;
+    }
+
+    vector<string> readBinaryWatch(int num){
+        vector<string> result;
+        for (int h = 0; h < 12; ++h){
+            for (int m = 0; m < 60; ++m){
+                if (bit_count(h) + bit_count(m) == num){
+                    const string hour = to_string(h);
+                    const string minute = m < 10 ? "0" + to_string(m) : to_string(m);
+                    result.emplace_back(hour + ":" + minute);
+                }
+            }
+        }
+        return result;
+    }
+};
