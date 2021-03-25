ad4bfa4f25982859cc83537e7eb835eb6439faa4
gargimahale <gargimahale102@gmail.com>
2021-03-25 16:38:43 -0400

    Added LeetCode Files

--- LeetCode/Medium/String to Integer (atoi).cpp
+++ LeetCode/Medium/String to Integer (atoi).cpp
@@ -1,28 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution{
 public:
-    int myAtoi(string s) {
-        int i = 0, n = s.size();
-        int sign = 1;
-        long ans = 0;
-        if (s.length() == 0) return 0;
+    int myAtoi(string s){
+        long res = 0;
 
-        //Discard whitespaces in the beginning
-        while (i < s.length() && s[i] == ' ')
+        int i = 0, sign = 1;
+        if (s.size() == 0)
+            return 0;
+
+        while (i < s.size() && s[i] == ' ')
             i++;
 
-        // Check if optional sign if it exists
-        if (i < s.length() && (s[i] == '+' || s[i] == '-'))
+        if (i < s.size() && (s[i] == '+' || s[i] == '-'))
             sign = (s[i++] == '-') ? -1 : 1;
 
-        // Build the result and check for overflow/underflow condition
-        while(i < n && isdigit(s[i])){
-            ans = ans*10 + (s[i++]-'0');
-            if (ans * sign >= INT_MAX) return INT_MAX;
-            if (ans * sign <= INT_MIN) return INT_MIN;
+        while (i < s.size() && isdigit(s[i])){
+            // proving bounds
+            res = res * 10 + (s[i++] - '0');
+            if (res * sign >= INT_MAX)
+                return INT_MAX;
+            if (res * sign <= INT_MIN)
+                return INT_MIN;
         }
-        return ans*sign;
+        return res * sign;
     }
 };
