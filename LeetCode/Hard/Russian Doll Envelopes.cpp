0137164c16b76e74a60c02e83a0296f21fd64f29
gargimahale <gargimahale102@gmail.com>
2021-03-31 01:41:12 -0400

    Added LeetCode Files

--- LeetCode/Hard/Russian Doll Envelopes.cpp
+++ LeetCode/Hard/Russian Doll Envelopes.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Tc: O(nlogn), SC: O(n)
+
 class Solution{
 public:
     int maxEnvelopes(vector<vector<int> >& env){
@@ -13,7 +15,7 @@ public:
         });
 
         vector<int> dp;
-        for (auto x : env){
+        for (auto x: env){
             auto it = lower_bound(begin(dp), end(dp), x[1]);
             if (it == dp.end()) dp.push_back(x[1]);
             else if (x[1] < *it) *it = x[1];
