28cd8e4e91381b229af6c5d3f7f0eef6448b064f
gargimahale <gargimahale102@gmail.com>
2021-03-30 22:18:29 -0400

    Added LeetCode Files

---
+++ LeetCode/Medium/Distribute Coins in Binary Tree.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespacs std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution{
+public:
+    int res = 0;
+
+    int traversal(TreeNode *root){
+        if (root->left){
+            root->val += traversal(root->left);
+        }
+
+        if (root->right){
+            root->val += traversal(root->right);
+        }
+
+        res += abs(root->val - 1);
+        return root->val - 1;
+    }
+
+    int distributeCoins(TreeNode *root){
+        traversal(root);
+        return res;
+    }
+};
