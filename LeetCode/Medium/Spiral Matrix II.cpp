cb60f8eeac30d256987e78f84220996e81c1d89f
gargimahale <gargimahale102@gmail.com>
2021-03-31 21:59:58 -0400

    Added LeetCode Files

---
+++ LeetCode/Medium/Spiral Matrix II.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// TC: O(n^2), SC: O(1)
+
+class Solution{
+public:
+    vector<vector<int>> generateMatrix(int n){
+        vector<vector<int>> matrix(n, vector<int>(n));
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int cnt = 1;
+
+        while (top <= bottom && left <= right){
+            for (int i = left; i <= right; ++i){
+                matrix[top][i] = cnt++;
+            }
+            ++top;
+
+            for (int i = top; i <= bottom; ++i){
+                matrix[i][right] = cnt++;
+            }
+            --right;
+
+            for (int i = right; i >= left; --i){
+                matrix[bottom][i] = cnt++;
+            }
+            --bottom;
+
+            for (int i = bottom; i >= top; --i){
+                matrix[i][left] = cnt++;
+            }
+            ++left;
+        }
+        return matrix;
+    }
+};
