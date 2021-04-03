3882ffd68e922871c81de5af183ca7c670797fb9
gargimahale <gargimahale102@gmail.com>
2021-04-02 23:30:37 -0400

    Added Codeforces Files

--- Codeforces/April Fools Day Contest 2021/Xenolith? Hippodrome?.cpp
+++ Codeforces/April Fools Day Contest 2021/Xenolith? Hippodrome?.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main(){
-    
+    int n, m;
+    cin >> n >> m;
+    set<int> S;
+    while(n){
+        if (!S.insert(n%m).second){
+            cout << "NO\n";
+            return 0;
+        }
+        n /= m;
+    }
+    cout << "YES\n";
 }
