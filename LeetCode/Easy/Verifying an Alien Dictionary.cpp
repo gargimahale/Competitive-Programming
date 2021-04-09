2c02eed59e47fcb3f7cc40909deb0a44c7cc114c
gargimahale <gargimahale102@gmail.com>
2021-04-09 19:18:36 -0400

    Added LeetCode Files

---
+++ LeetCode/Easy/Verifying an Alien Dictionary.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isAlienSorted(vector<string> &words, string order)
+    {
+        int mapping[26];
+        for (int i = 0; i < 26; ++i)
+        {
+            mapping[order[i] - 'a'] = i;
+        }
+
+        for (auto &w : words)
+        {
+            for (char &ch : w)
+            {
+                ch = mapping[ch - 'a'];
+            }
+        }
+        return is_sorted(begin(words), end(words));
+    }
+};
