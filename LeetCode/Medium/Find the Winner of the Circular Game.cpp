d5288c95264336e4319559fd342297ca7975666e
gargimahale <gargimahale102@gmail.com>
2021-04-11 00:23:51 -0400

    Added LeetCode Files

--- LeetCode/Medium/Find the Winner of the Circular Game.cpp
+++ LeetCode/Medium/Find the Winner of the Circular Game.cpp
@@ -4,15 +4,15 @@ using namespace std;
 class Solution{
 public:
     int findTheWinner(int n, int k){
-        vector<int> A;
-        for (int i = 1; i <= n; ++i){
-            A.push_back(i);
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i)
+            a[i] = i + 1;
+
+        int at = 0;
+        while (a.size() > 1){
+            at = (at + k - 1) % a.size();
+            a.erase(begin(a) + at);
         }
-        int idx = k - 1;
-        for (int i = 0; i < n - 1; ++i){
-            A.erase(A.begin() + idx);
-            idx = (idx + k - 1) % A.size();
-        }
-        return A[0];
+        return a[0];
     }
 };
