048af34fbd362d0f98807bef3e365788be2444e5
gargimahale <gargimahale102@gmail.com>
2021-04-11 02:01:40 -0400

    Added LeetCode Files

---
+++ LeetCode/Hard/Longest Increasing Path in a Matrix.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int dfs(vector<vector<int>> &m, int i, int j, int v, vector<vector<int>> &len){
+        if (i < 0 || j < 0 || i == m.size() || j == m[0].size() || m[i][j] <= v)
+            return 0;
+        return len[i][j] > 0 ? len[i][j] : len[i][j] = 1 + max(max(dfs(m, i - 1, j, m[i][j], len), dfs(m, i + 1, j, m[i][j], len)), max(dfs(m, i, j - 1, m[i][j], len), dfs(m, i, j + 1, m[i][j], len)));
+    }
+
+    int longestIncreasingPath(vector<vector<int>> &m, int res = 0){
+        vector<vector<int>> len(m.size(), vector<int>(m.size() == 0 ? 0 : m[0].size()));
+        for (auto i = 0; i < m.size(); ++i)
+            for (auto j = 0; j < m[0].size(); ++j)
+                res = max(res, dfs(m, i, j, m[i][j] - 1, len));
+        return res;
+    }
+};
