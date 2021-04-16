69a72b7627e9aaa5a1a3e7d2685c701030ac209a
gargimahale <gargimahale102@gmail.com>
2021-04-16 17:50:52 -0400

    Added LeetCode Files

--- LeetCode/Medium/remove_all_adj_duplicates_in_str_2.cpp
+++ LeetCode/Medium/remove_all_adj_duplicates_in_str_2.cpp
@@ -3,14 +3,13 @@ using namespace std;
 
 string removeDuplicates(string s, int k) {
     stack<int> count;
-    for (int i=0; i<s.size(); ++i){
-        if (i == 0 || s[i] != s[i-1]){
+    for (int i = 0; i < s.size(); ++i) {
+        if (i == 0 || s[i] != s[i - 1]) {
             count.push(1);
         }
-        else if (++count.top() == k){
-            cout << count.top() << "\n";
+        else if (++count.top() == k) {
             count.pop();
-            s.erase(i-k+1, k);
+            s.erase(i - k + 1, k);
             i = i - k;
         }
     }
