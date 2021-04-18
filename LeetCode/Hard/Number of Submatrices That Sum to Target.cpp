3af38a0ea802861b69ae7c3a07bf63cd1ef01315
gargimahale <gargimahale102@gmail.com>
2021-04-18 02:15:49 -0400

    Added LeetCode Files

--- LeetCode/Hard/Number of Submatrices That Sum to Target.cpp
+++ LeetCode/Hard/Number of Submatrices That Sum to Target.cpp
@@ -1,6 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-
-}
+class Solution {
+public:
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size(), cols = matrix[0].size();
+        int count = 0;
+        for (int L = 0; L < cols; L++) {
+            //intialize the running row
+            vector<int> running_rows(rows, 0);
+            //calculate running rows sum
+            for (int R = L; R < cols; R++) {
+                for (int i = 0; i < rows; i++) running_rows[i] += matrix[i][R];
+                int sum = 0;
+                unordered_map<int, int> visited;
+                //check for subarrays in every running row
+                for (int i = 0; i < rows; i++) {
+                    visited[sum]++;
+                    sum = sum + running_rows[i];
+                    if (visited[sum - target])
+                        count += visited[sum - target];
+                }
+            }
+        }
+        return count;
+    }
+};
