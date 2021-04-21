8031646e844980c61b05ecf85d6a14ba52c8d25f
gargimahale <gargimahale102@gmail.com>
2021-04-21 00:23:10 -0400

    Added Leetcode Files

---
+++ LeetCode/Easy/N-ary Tree Preorder Traversal.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> res;
+    
+    void travel(Node* root){
+        if (root == NULL){
+            return;
+        }
+        
+        res.push_back(root->val);
+        for(auto n: root->children){
+            travel(n);
+        }
+    }
+    
+    vector<int> preorder(Node* root) {
+        travel(root);
+        return res;
+    }
+};
