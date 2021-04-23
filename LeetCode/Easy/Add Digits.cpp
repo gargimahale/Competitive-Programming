b6bb0d2ba581622f332ee376ade1fbb10249c060
gargimahale <gargimahale102@gmail.com>
2021-04-23 17:48:13 -0400

    Added Leetcode Files

---
+++ LeetCode/Easy/Add Digits.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+    int helper(int num){
+        int sum = 0;
+        while(num){
+            sum += (num%10);
+            num /= 10;
+        }
+        return sum;
+    }
+    
+    int addDigits(int num) {
+        if (num <= 9) return num;
+        while(true){
+            num = helper(num);
+            if (num <= 9) return num;
+        }
+        return num;
+    }
+};
