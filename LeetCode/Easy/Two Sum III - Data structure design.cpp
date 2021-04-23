7de5bccdd83013688576a4c1a3e3043cea61e1c9
gargimahale <gargimahale102@gmail.com>
2021-04-23 05:06:29 -0400

    Added Leetcode Files

---
+++ LeetCode/Easy/Two Sum III - Data structure design.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TwoSum {
+public:
+    unordered_map<int, int> mp;
+    /** Initialize your data structure here. */
+    TwoSum() {}
+    
+    /** Add the number to an internal data structure.. */
+    void add(int number) {
+        mp[number]++;
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    bool find(int value) {
+        if (value >= INT_MAX || value <= INT_MIN) return false;
+        
+        for (auto it = mp.begin(); it != mp.end(); ++it){
+            int x = it->first;
+            int y = value - x;
+            if ((x == y && it->second > 1) || (x != y && mp.find(y) != mp.end())){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum* obj = new TwoSum();
+ * obj->add(number);
+ * bool param_2 = obj->find(value);
+ */
