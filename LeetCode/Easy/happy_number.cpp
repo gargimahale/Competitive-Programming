c0585a54eb61dd0acc8d0ef389380ca4cd41442b
gargimahale <gargimahale102@gmail.com>
2021-04-23 17:29:02 -0400

    Added Leetcode Files

--- LeetCode/Easy/happy_number.cpp
+++ LeetCode/Easy/happy_number.cpp
@@ -2,32 +2,26 @@
 
 using namespace std;
 
-int f(int n){
-    int sum=0;
-    while(n){
-        int digit = n%10;
-        n/=10;
-        sum+=(digit*digit);
+class Solution {
+public:
+    
+    int helper(int n){
+        int sum = 0;
+        while(n){
+            sum += ((n%10) * (n%10));
+            n/=10;
+        }
+        return sum;
     }
-    return sum;
-}
-
-bool solve(int n) {
-    unordered_set<int> visited;
-    while(true){
-        if (n==1) return true;
-        n = f(n);
-        if(visited.count(n)==1) return false;
-        visited.insert(n);
+    
+    bool isHappy(int n) {
+        unordered_set<int> S;
+        while(true){
+            if (n == 1) return true;
+            n = helper(n);
+            if (S.count(n) > 0) return false;
+            S.insert(n);
+        }
+        return false;
     }
-}
-
-int main() {
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int t = 1,n;
-    cin >> n;
-    while(t--) {
-      	cout << solve(n) << "\n";
-    }
-    return 0;
-}
+};
