fe6ac6db5713608627f0731c86fc2443727446c7
gargimahale <gargimahale102@gmail.com>
2021-04-24 21:20:46 -0400

    Added Leetcode Files

---
+++ LeetCode/Easy/Logger Rate Limiter.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Logger {
+public:
+    /** Initialize your data structure here. */
+    unordered_map<string, int> log;
+    
+    Logger() {}
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    bool shouldPrintMessage(int timestamp, string message) {
+        if (timestamp < log[message]) return false;
+        log[message] = timestamp+10;
+        return true;
+    }
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger* obj = new Logger();
+ * bool param_1 = obj->shouldPrintMessage(timestamp,message);
+ */
+
+int main(){
+    Logger s;
+    for(int i=0; i<10; ++i){
+        int time;
+        string message;
+        cin >> time >> message;
+        cout << s.shouldPrintMessage(time, message) << endl;
+    }
+    return 0;
+}
---
+++ LeetCode/Medium/Longest Increasing Subsequence.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int lengthOfLIS(vector<int>& nums){
+        vector<int> res;
+        for (int i=0; i<nums.size(); ++i){
+            auto it = lower_bound(begin(res), end(res), nums[i]);
+            if (it == res.end()){
+                res.push_back(nums[i]);
+                cout << nums[i] << "\n";
+            }
+            else{
+                *it = nums[i];
+                cout << "change: " << *it << "\n"; 
+            }
+        }
+
+        for (int x: res) cout << x << " ";
+        cout << "\n";
+        return res.size();
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> a = {0, 1, 0, 3, 2, 3};
+    cout << s.lengthOfLIS(a) << "\n";
+    return 0;
+}
