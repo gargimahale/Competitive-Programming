4ee914b4a7922d796ad59dbf4a17e2cb7f3919dc
gargimahale <gargimahale102@gmail.com>
2021-05-03 00:49:22 -0400

    Added Leetcode Files

---
+++ LeetCode/SQL/Easy/ Sales Analysis II.sql
@@ -0,0 +1,10 @@
+select distinct buyer_id
+from sales
+join product using(product_id)
+where product_name = 'S8'
+and buyer_id not in (
+    select distinct buyer_id
+    from sales
+    join product using(product_id)
+    where product_name = 'iphone'
+)
