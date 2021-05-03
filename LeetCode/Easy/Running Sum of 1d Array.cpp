3d5db76ef626ab4e4a47490f69d59729938532d4
gargimahale <gargimahale102@gmail.com>
2021-05-03 17:43:12 -0400

    Added Leetcode Files

---
+++ LeetCode/Easy/Running Sum of 1d Array.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tc: O(n), SC: O(1)
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        if (nums.size() == 0) return nums;
+        for (int i = 1; i<nums.size(); ++i){
+            nums[i] = nums[i]+nums[i-1];
+        }
+        return nums;
+    }
+};
