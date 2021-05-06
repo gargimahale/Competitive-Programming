5d52a9db05de2eb83862af9765f3573bd0b285dd
gargimahale <gargimahale102@gmail.com>
2021-05-06 00:18:56 -0400

    Added LeetCode Files

--- LeetCode/Medium/Jump_Game.cpp
+++ LeetCode/Medium/Jump_Game.cpp
@@ -5,17 +5,14 @@ using namespace std;
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size(), step = nums[0];
-        if (n == 0){
-            return false;
-        }
-        for(int i=1; i<n; ++i){
-            step--;
-            if (step < 0){
-                return false;
-            }
-            if (nums[i] > step){
-                step = nums[i];
+        int n = nums.size();
+        int step = nums[0];
+        for (int i = 1; i < n; ++i) {
+            steps--;
+            if (steps < 0) return false;
+
+            if (nums[i] > steps) {
+                steps = nums[i];
             }
         }
         return true;
--- LeetCode/Medium/word_search.cpp
+++ LeetCode/Medium/word_search.cpp
@@ -5,11 +5,11 @@ using namespace std;
 class Solution {
 public:
 
-    bool dfs(vector<vector<char>>& board, string& word, int u, int x, int y){
-        if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size() || board[x][y]!= word[u]){
+    bool dfs(vector<vector<char>>& board, string& word, int u, int x, int y) {
+        if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size() || board[x][y] != word[u]) {
             return false;
         }
-        if (u == word.size()-1){
+        if (u == word.size() - 1) {
             return true;
         }
 
@@ -19,8 +19,8 @@ public:
         // mark as visited
         board[x][y] = ' ';
 
-        found = dfs(board, word, u+1, x, y-1) || dfs(board, word, u+1, x, y+1) || dfs(board, word, u+1, x+1, y) ||
-            dfs(board, word, u+1, x-1, y);
+        found = dfs(board, word, u + 1, x, y - 1) || dfs(board, word, u + 1, x, y + 1) || dfs(board, word, u + 1, x + 1, y) ||
+                dfs(board, word, u + 1, x - 1, y);
 
         board[x][y] = temp;
         return found;
@@ -30,9 +30,9 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         if (!board.size()) return false;
         int row = board.size(), col = board[0].size();
-        for (int i=0; i<row; ++i){
-            for (int j=0; j<col; ++j){
-                if (board[i][j] == word[0] && dfs(board, word, 0, i, j)){
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (board[i][j] == word[0] && dfs(board, word, 0, i, j)) {
                     return true;
                 }
             }
