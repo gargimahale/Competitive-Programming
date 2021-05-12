9ca0c2a6f6ef4d0f3fd60cd7c90eccdd91c03b26
gargimahale <gargimahale102@gmail.com>
2021-05-12 01:52:25 -0400

    Added LeetCode Files

---
+++ LeetCode/Medium/Maximum Points You Can Obtain from Cards.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int res = 0, n = cardPoints.size(), t = 0;
+        for (int i = n - k; i < n; ++i) res += cardPoints[i];
+        for (int i = 0, t = res; i < k; ++i) {
+            t -= cardPoints[n - k + i];
+            t += cardPoints[i];
+            res = max(res, t);
+        }
+        return res;
+    }
+};
