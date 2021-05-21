24f0c6e2963b249aeda9bd7d7804540bcc50facb
gargimahale <gargimahale102@gmail.com>
2021-05-21 12:06:39 -0400

    Updated Leetcode File

--- LeetCode/Hard/trapping_rainwater.cpp
+++ LeetCode/Hard/trapping_rainwater.cpp
@@ -1,16 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int l = 0, r = height.size()-1, lvl = 0, water = 0;
-        while(l<r){
-            int lower = height[height[l] < height[r] ? l++:r--];
-            lvl = max(lvl, lower);
-            water += (lvl-lower);
+        int l = 0, r = height.size() - 1, water = 0, minHeight = 0;
+        while (l < r) {
+            while (l < r && height[l] <= minHeight) {
+                water += minHeight - height[l++];
+            }
+            while (l < r && height[r] <= minHeight) {
+                water += minHeight - height[r--];
+            }
+            minHeight = min(height[l], height[r]);
         }
         return water;
     }
-}; 
+};
