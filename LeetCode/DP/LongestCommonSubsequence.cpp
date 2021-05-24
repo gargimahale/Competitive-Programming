ad58841288aac93a29a7f5ced5e7a0aeab2ff96e
gargimahale <gargimahale102@gmail.com>
2021-05-23 23:55:47 -0400

    Updated Leetcode File

---
+++ LeetCode/DP/LongestCommonSubsequence.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonSubsequence(string& a, string& b) {
+
+    }
+};
+
+int main(void) {
+    Solution sol;
+    string a = "", b = "";
+    cout << sol.longestCommonSubsequence(a, b);
+}
--- LeetCode/Easy/Add_to_Array-Form_of_Int.cpp
+++ LeetCode/Easy/Add_to_Array-Form_of_Int.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -6,13 +6,13 @@ class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& A, int K) {
         int n = A.size();
-        for(int i=n-1; i>=0 && K!=0; --i){
+        for (int i = n - 1; i >= 0 && K != 0; --i) {
             K += A[i];
-            A[i] = K%10;
+            A[i] = K % 10;
             K /= 10;
         }
-        while(K > 0){
-            A.insert(A.begin(), K%10);
+        while (K > 0) {
+            A.insert(A.begin(), K % 10);
             K /= 10;
         }
         return A;
