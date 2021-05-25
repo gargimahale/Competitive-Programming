0a9d437497a94970507e920fb8907cbbad46d9fa
gargimahale <gargimahale102@gmail.com>
2021-05-24 23:53:12 -0400

    Updated Leetcode File

--- LeetCode/Medium/Best Time to Buy and Sell Stock with Transaction Fee.cpp
+++ LeetCode/Medium/Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -1,7 +1,7 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-/** 
+/**
  * s0 = profit having no stock
  * s1 = profit having 1 stock
  * update s0 by selling the stock from s1, so s0 = max(s0, s1+p);
@@ -12,10 +12,10 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int s0 = 0, s1 = INT_MIN;
-        for(int x: prices){
+        for (int x : prices) {
             int temp = s0;
-            s0 = max(s0, s1+x);
-            s1 = max(s1, temp-x-fee);
+            s0 = max(s0, s1 + x);
+            s1 = max(s1, temp - x - fee);
         }
         return s0;
     }
