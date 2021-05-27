68ab6af049243de5ae8c79db754519058bb00654
gargimahale <gargimahale102@gmail.com>
2021-05-27 16:49:01 -0400

    Updated LeetCode Files

--- LeetCode/Medium/subarray_sum_k.cpp
+++ LeetCode/Medium/subarray_sum_k.cpp
@@ -7,15 +7,27 @@ using namespace std;
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size(), ans = 0, pref = 0;
-        unordered_map<int, int> cntPref;
-        cntPref[pref]++;
-        for (int i = 0; i < n; ++i) {
-            pref += nums[i];
-            int need = pref - k;
-            ans += cntPref[need];
-            cntPref[pref]++;
+        int n = nums.size();
+        if (n == 0) return 0;
+
+        // <prefixSum, Count>
+        unordered_map<int, int> mp;
+        int count = 0, prefixSum = 0;
+
+        int i = 0;
+        while (i < n) {
+            prefixSum += nums[i];
+            if (prefixSum == k) {
+                ++count;
+            }
+
+            if (mp.find(prefixSum - k) != mp.end()) {
+                count += mp[prefixSum - k];
+            }
+
+            mp[prefixSum]++;
+            ++i;
         }
-        return ans;
+        return count;
     }
 };
