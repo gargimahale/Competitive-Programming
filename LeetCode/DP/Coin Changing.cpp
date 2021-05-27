2e92c3abb2b32b3af7cfa8863dd4c5b22cbd2e59
gargimahale <gargimahale102@gmail.com>
2021-05-27 12:48:29 -0400

    Added DP Files

---
+++ LeetCode/DP/Coin Changing.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+
+        vector<int> dp (amount + 1, amount + 1);
+
+        sort(begin(coins), end(coins));
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+
+            for (int j = 0; j < coins.size(); ++j) {
+
+                if (i >= coins[j]) {
+                    dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
+                }
+                else {
+                    // if coin is much larger than the curr amount
+                    break;
+                }
+            }
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount] ;
+    }
+};
+
+int main(void) {
+    vector<int> den = {1, 5, 6, 8};
+    int total = 11;
+    cout << minCoinsRequired(den, total) << "\n";
+}
+
+// Tc: O(amount * denominations), SC: O(amount)
