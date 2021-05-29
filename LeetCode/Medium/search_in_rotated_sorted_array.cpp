4653e8d2b95faf88d1aada60663e3830195bd2ba
gargimahale <gargimahale102@gmail.com>
2021-05-29 12:57:21 -0400

    Updated Leetcode Files

--- LeetCode/Medium/search_in_rotated_sorted_array.cpp
+++ LeetCode/Medium/search_in_rotated_sorted_array.cpp
@@ -5,27 +5,29 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size()-1;
-        while(low <= high){
-            int mid = low+(high-low)/2;
-            if (nums[mid] == target){
+        int low = 0, high = nums.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
                 return mid;
             }
+
             // Search in the right half
-            if (nums[mid] <= nums[high]){
-                if (target > nums[mid] && target <= nums[high]){
-                    low = mid+1;
+            if (nums[mid] <= nums[high]) {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
                 }
-                else{
-                    high = mid-1;
+                else {
+                    high = mid - 1;
                 }
             }
-            else{
-                if (target < nums[mid] && target >= nums[low]){
-                    high = mid -1;
+            // search in the left half
+            else {
+                if (target < nums[mid] && target >= nums[low]) {
+                    high = mid - 1;
                 }
-                else{
-                    low = mid+1;
+                else {
+                    low = mid + 1;
                 }
             }
         }
