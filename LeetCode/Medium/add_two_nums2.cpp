c7d1bd532a8268772c2343e8bc1852bc370508b2
gargimahale <gargimahale102@gmail.com>
2021-05-30 00:30:45 -0400

    Updated LeetCode Files

--- LeetCode/Medium/add_two_nums2.cpp
+++ LeetCode/Medium/add_two_nums2.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 
 //  * Definition for singly-linked list.
 struct ListNode {
@@ -15,9 +13,9 @@ struct ListNode {
 class Solution {
 public:
 
-    ListNode* reverseList(ListNode* p){
+    ListNode* reverseList(ListNode* p) {
         ListNode* curr = nullptr;
-        while(p){
+        while (p) {
             ListNode* temp = p->next;
             p->next = curr;
             curr = p;
@@ -32,19 +30,19 @@ public:
 
         ListNode* p = nullptr;
         int c = 0;
-        while(l1 || l2){
-            if (l1){
+        while (l1 || l2) {
+            if (l1) {
                 c += l1->val;
                 l1 = l1->next;
             }
-            if (l2){
+            if (l2) {
                 c += l2->val;
                 l2 = l2->next;
             }
-            p = new ListNode(c%10, p);
+            p = new ListNode(c % 10, p);
             c /= 10;
         }
-        if (c){
+        if (c) {
             return new ListNode(c, p);
         }
         return p;
