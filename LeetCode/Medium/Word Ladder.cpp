ae65661ecd2c7017a392a98bf2426a45f6040747
gargimahale <gargimahale102@gmail.com>
2021-06-01 13:36:52 -0400

    Updated LeetCode Files

--- LeetCode/Medium/Word Ladder.cpp
+++ LeetCode/Medium/Word Ladder.cpp
@@ -11,36 +11,36 @@ public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set<string> map;
         bool found = false;
-        for (auto w: wordList){
-            if (w == endWord){
+        for (auto w : wordList) {
+            if (w == endWord) {
                 found = true;
             }
             map.insert(w);
         }
-        if(!found){
+        if (!found) {
             return 0;
         }
-        
+
         queue<string> pendingNodes;
         pendingNodes.push(beginWord);
         int lvl = 0;
-        while(pendingNodes.size() != 0){
+        while (pendingNodes.size() != 0) {
             lvl++;
             int size = pendingNodes.size();
-            for (int i=0; i<size; ++i){
+            for (int i = 0; i < size; ++i) {
                 string current = pendingNodes.front();
                 pendingNodes.pop();
-                for(int j=0; j<current.size(); ++j){
+                for (int j = 0; j < current.size(); ++j) {
                     string temp = current;
-                    for (char c = 'a'; c<='z'; ++c){
+                    for (char c = 'a'; c <= 'z'; ++c) {
                         temp[j] = c;
-                        if (temp == current){
+                        if (temp == current) {
                             continue;
                         }
-                        else if (endWord == temp){
-                            return lvl+1;
+                        else if (endWord == temp) {
+                            return lvl + 1;
                         }
-                        else if(map.find(temp) != map.end()){
+                        else if (map.find(temp) != map.end()) {
                             pendingNodes.push(temp);
                             map.erase(temp);
                         }
