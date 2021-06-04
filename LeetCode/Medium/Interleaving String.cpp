5b30b8e3fb76fbaa5f845e5abe4607071ff47c84
gargimahale <gargimahale102@gmail.com>
2021-06-04 13:29:01 -0400

    Added LeetCode Files

---
+++ LeetCode/Medium/Interleaving String.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int a = s1.length(), b = s2.length(), c = s3.length();
+
+        if (a + b != c) {
+            return false;
+        }
+
+        bool table[a + 1][b + 1];
+
+        for (int i = 0; i < a + 1; i++)
+            for (int j = 0; j < b + 1; j++) {
+                if (i == 0 && j == 0)
+                    table[i][j] = true;
+                else if (i == 0)
+                    table[i][j] = ( table[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
+                else if (j == 0)
+                    table[i][j] = ( table[i - 1][j] && s1[i - 1] == s3[i + j - 1]);
+                else
+                    table[i][j] = (table[i - 1][j] && s1[i - 1] == s3[i + j - 1] ) || (table[i][j - 1] && s2[j - 1] == s3[i + j - 1] );
+            }
+        return table[s1.length()][s2.length()];
+    }
+};
