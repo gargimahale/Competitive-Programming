2c9d3bef6b7e5950efa1803b47df85372a00beda
gargimahale <gargimahale102@gmail.com>
2021-06-13 18:24:08 -0400

    Added Leetcode Files

--- LeetCode/Medium/Max Area of Island.cpp
+++ LeetCode/Medium/Max Area of Island.cpp
@@ -3,24 +3,36 @@ using namespace std;
 
 class Solution {
 public:
+	void dfs(vector<vector<int>>& grid, int i, int j, int& sum) {
+		if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0) return;
 
-	int areaOfLand(vector<vector<int>>& grid, int i, int j) {
-		if (i >= 0 && i < grid.size() && j >= 0 && j < grid[0].size() && grid[i][j] == 1) {
-			grid[i][j] = 0;
-			return 1 + areaOfLand(grid, i + 1, j) + areaOfLand(grid, i - 1, j) + areaOfLand(grid, i, j - 1) + areaOfLand(grid, i, j + 1);
-		}
-		return 0;
+		grid[i][j] = 0;
+		++sum;
+
+		dfs(grid, i - 1, j, sum);
+		dfs(grid, i + 1, j, sum);
+		dfs(grid, i, j - 1, sum);
+		dfs(grid, i, j + 1, sum);
 	}
 
 	int maxAreaOfIsland(vector<vector<int>>& grid) {
-		int ans = 0;
-		for (int i = 0; i < grid.size(); ++i) {
-			for (int j = 0; j < grid[0].size(); ++j) {
+		int n = grid.size(), m = grid[0].size();
+		int maxArea = 0, sum = 0;
+
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
 				if (grid[i][j] == 1) {
-					ans = max(ans, areaOfLand(grid, i, j));
+					sum = 0;
+					dfs(grid, i, j, sum);
+					if (sum > maxArea) {
+						maxArea = sum;
+					}
 				}
 			}
 		}
-		return ans;
+		return maxArea;
 	}
 };
+
+// TC: O(R*C)
+// SC: O(R*C);
