3d94167153ad480e1820cd63ceb2cc693490039f
gargimahale <gargimahale102@gmail.com>
2021-06-14 21:56:21 -0400

    Added Leetcode Files

--- LeetCode/Easy/implement_Str().cpp
+++ LeetCode/Easy/implement_Str().cpp
@@ -6,16 +6,16 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         if (haystack.size() < needle.size()) return -1;
-        int idx=0, i,j;
-        for(i=0, j=0; i<haystack.size() && j<needle.size();){
-            if(haystack[i]==needle[j]) i++, j++;
-            else{
+        int idx = 0, i, j;
+        for (i = 0, j = 0; i < haystack.size() && j < needle.size();) {
+            if (haystack[i] == needle[j]) i++, j++;
+            else {
                 idx++;
-                i=idx;
-                j=0;
+                i = idx;
+                j = 0;
             }
         }
-        if(j == needle.size()) return idx;
+        if (j == needle.size()) return idx;
         return -1;
     }
 };
