c60e85e3c84879853a0784642e2ac28436152665
gargimahale <gargimahale102@gmail.com>
2021-06-16 13:42:20 -0400

    Added Leetcode Files

--- LeetCode/Medium/Find the Celebrity.cpp
+++ LeetCode/Medium/Find the Celebrity.cpp
@@ -7,23 +7,23 @@ using namespace std;
 class Solution {
 public:
     int findCelebrity(int n) {
-        int i=0; 
-        
+        int i = 0;
+
         // finding i who does not know j
-        for (int j=1; j<n; ++j){
+        for (int j = 1; j < n; ++j) {
             if (knows(i, j)) i = j;
         }
-        
+
         // check if i does not know j
-        for (int j=0; j<i; ++j){
+        for (int j = 0; j < i; ++j) {
             if (knows(i, j)) return -1;
         }
-        
+
         // check if each person knows i and i != j
-        for (int j=0; j<n; ++j){
+        for (int j = 0; j < n; ++j) {
             if (i != j && !knows(j, i)) return -1;
         }
-        
+
         return i;
     }
 };
