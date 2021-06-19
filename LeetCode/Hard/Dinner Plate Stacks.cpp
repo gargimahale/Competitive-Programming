a32ed56e04218ec4d209017965fa61c952fc6406
gargimahale <gargimahale102@gmail.com>
2021-06-19 14:41:04 -0400

    Added Leetcode Files

--- LeetCode/Hard/Dinner Plate Stacks.cpp
+++ LeetCode/Hard/Dinner Plate Stacks.cpp
@@ -6,56 +6,56 @@ class DinnerPlates {
 public:
     vector<vector<int>> v;
     int cap, l, r, out;
-    
+
     DinnerPlates(int capacity) {
         v = vector<vector<int>> (200001);
         cap = capacity;
         l = 0, r = 0;
     }
-    
+
     void push(int val) {
         v[l].push_back(val);
-        while(v[l].size() == cap){
+        while (v[l].size() == cap) {
             ++l;
         }
-        
-        while(!v[r].empty()){
+
+        while (!v[r].empty()) {
             ++r;
         }
         --r;
     }
-    
+
     int pop() {
-        if (v[r].size() == 0 || r < 0){
+        if (v[r].size() == 0 || r < 0) {
             return -1;
         }
-        
+
         out = v[r].back();
         v[r].pop_back();
-        
-        while(v[r].empty() && r > 0){
+
+        while (v[r].empty() && r > 0) {
             --r;
         }
-        
-        if (l > r){
+
+        if (l > r) {
             l = r;
         }
         return out;
     }
-    
+
     int popAtStack(int index) {
-        if (v[index].size() == 0){
+        if (v[index].size() == 0) {
             return -1;
         }
-        
+
         int out = v[index].back();
         v[index].pop_back();
-        
-        if (l > index){
+
+        if (l > index) {
             l = index;
         }
-        
-        while(v[index].empty() && r == index && r > 0){
+
+        while (v[index].empty() && r == index && r > 0) {
             --r;
         }
         return out;
