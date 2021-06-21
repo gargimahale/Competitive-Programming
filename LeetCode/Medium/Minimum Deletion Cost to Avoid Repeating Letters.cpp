c99272e2a715fa285ef7c33d666810193dc04661
gargimahale <gargimahale102@gmail.com>
2021-06-21 13:01:16 -0400

    Added Leetcode Files

--- LeetCode/Medium/Minimum Deletion Cost to Avoid Repeating Letters.cpp
+++ LeetCode/Medium/Minimum Deletion Cost to Avoid Repeating Letters.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-    int minCost(string s, vector<int> &cost){
+    int minCost(string s, vector<int> &cost) {
         int res = 0, maxCost = 0, n = s.size();
-        for (int i = 0; i < n; ++i){
+        for (int i = 0; i < n; ++i) {
             if (i > 0 && s[i] != s[i - 1])
                 maxCost = 0;
             res += min(maxCost, cost[i]);
---
+++ LeetCode/Medium/Remove Comments.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// TC: O(N), SC: O(K)
+// Where K is the max length of the line
+
+class Solution {
+public:
+	vector<string> removeComments(vector<string>& source) {
+		int len = source.size();
+		if (len == 0) return {};
+
+		vector <string> ans;
+		string build;
+		bool block = false;
+
+		for (string line : source) {
+			for (int i = 0, m = line.size(); i < m; ++i) {
+				string two = line.substr(i, 2);
+				if (!block) {
+					if (two == "//") break;
+					else if (two == "/*") block = true, ++i;
+					else build.push_back(line[i]);
+				}
+				else if (two == "*/") block = false, ++i;
+			}
+
+			if (!block && build.size()) ans.push_back(build), build.clear();
+		}
+
+		return ans;
+	}
+};
