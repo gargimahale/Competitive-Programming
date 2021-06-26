acf650be28ae955b1831a4454611a09edfd0c1be
gargimahale <gargimahale102@gmail.com>
2021-06-26 17:50:48 -0400

    Added Leetcode Files

--- LeetCode/Medium/subsets.cpp
+++ LeetCode/Medium/subsets.cpp
@@ -21,23 +21,23 @@ using namespace std;
 
 class Solution {
 public:
-    
+
     vector<vector<int>> result;
-    
-    void helperSubsets(vector<int>& nums, vector<int> temp, int start, int end){
-        if (start == end){
+
+    void helperSubsets(vector<int>& nums, vector<int> temp, int start, int end) {
+        if (start == end) {
             return;
         }
-        
-        for(int i=start; i<end; ++i){
+
+        for (int i = start; i < end; ++i) {
             temp.push_back(nums[i]);
             result.push_back(temp);
-            helperSubsets(nums, temp, i+1, end);
+            helperSubsets(nums, temp, i + 1, end);
             temp.pop_back();
         }
     }
-    
-    
+
+
     vector<vector<int>> subsets(vector<int>& nums) {
         result.push_back({});
         helperSubsets(nums, {}, 0, nums.size());
@@ -45,30 +45,31 @@ public:
     }
 };
 
-// vector<vector<int>> subsets(vector<int>& nums) {
-//     int n = nums.size();
-//     int p = 1 << n;
-//     vector<vector<int>> retVec(p, vector<int>{});
-//     for (int i = 1; i<p; ++i){
-//         int mask = 0x01;
-//         int index = 0;
-//         while(i >= mask){
-//             if(mask == (i & mask)){
-//                 retVec[i].pb(nums[index]);
-//             }
-//             mask <<= 1;
-//             index++;
-//         }
-//     }
-//     return retVec; 
-// }
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subs = {{}};
 
-int main(){
+        int len = nums.size();
+
+        for (int num : nums) {
+            int n = subs.size();
+            for (int i = 0; i < n; ++i) {
+                subs.push_back(subs[i]);
+                subs.back().push_back(num);
+            }
+        }
+
+        return subs;
+    }
+};
+
+int main() {
     vector<int> nums = {1, 2, 3};
     vector<vector<int>> res = subsets(nums);
-    for (auto list: res){
+    for (auto list : res) {
         cout << '[';
-        for (auto ele: list){
+        for (auto ele : list) {
             cout << ele << "";
         }
         cout << ']' << endl;
