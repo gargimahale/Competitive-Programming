4cc13a44b5054e0b18245dc6bf1e9693bfd8e01c
gargimahale <gargimahale102@gmail.com>
2021-07-03 12:30:53 -0400

    Added Kattis Files

--- Kattis/test.cpp
+++ Kattis/test.cpp
@@ -1,8 +1,11 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    cout << "Hello World!";
-    return 0;
+int main(void) {
+    string a = "ab";
+
+    // starting form position 0 copy 0 chars and start copying chars from [i+1, ..]
+    cout << a.substr(0, 0) << " " << a.substr(0 + 1);
+    // cout << "Hello World!";
+    // return 0;
 }
