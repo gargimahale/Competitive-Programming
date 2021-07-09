9b1531feaef5324467a81e562d7058be469fa50b
gargimahale <gargimahale102@gmail.com>
2021-07-08 22:54:12 -0400

    Added LeetCode Files

--- LeetCode/Easy/first_bad_version.cpp
+++ LeetCode/Easy/first_bad_version.cpp
@@ -2,19 +2,20 @@
 using namespace std;
 
 int solve(int n) {
-    int left = 1, right = n;
-    while (left < right) {
+    int left = 1, right = n, res = -1;
+    while (left <= right) {
         int mid = left + ((right - left) / 2);
         if (isBadVersion(mid)) {
-            right = mid;
+            res = mid;
+            right = mid - 1;
         } else {
             left = mid + 1;
         }
     }
-    return left;
+    return res;
 }
 
-int main(int argc, char **argv) {
+int main() {
     int n;
     cin >> n;
     cout << solve(n);
