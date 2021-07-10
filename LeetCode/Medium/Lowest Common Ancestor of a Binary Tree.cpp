ccd63bd030083c3cdec67fcf8fa08b59d36308f5
gargimahale <gargimahale102@gmail.com>
2021-07-10 12:40:05 -0400

    Added LeetCode Files

--- LeetCode/Medium/Lowest Common Ancestor of a Binary Tree.cpp
+++ LeetCode/Medium/Lowest Common Ancestor of a Binary Tree.cpp
@@ -12,13 +12,13 @@ using namespace std;
  */
 class Solution {
 public:
-	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-		if (!root || root == p || root == q) return root;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q) return root;
 
-		TreeNode* left =  lowestCommonAncestor(root->left, p, q);
-		TreeNode* right =  lowestCommonAncestor(root->right, p, q);
+        TreeNode* left =  lowestCommonAncestor(root->left, p, q);
+        TreeNode* right =  lowestCommonAncestor(root->right, p, q);
 
-		return left && right ? root : (left ? left : right);
+        return left && right ? root : (left ? left : right);
 
-	}
+    }
 };
