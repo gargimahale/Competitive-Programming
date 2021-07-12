05bcb57d1c68288abb692b21910e4e90200a41af
gargimahale <gargimahale102@gmail.com>
2021-07-12 12:39:52 -0400

    Added LeetCode Files

--- LeetCode/Easy/Logger Rate Limiter.cpp
+++ LeetCode/Easy/Logger Rate Limiter.cpp
@@ -18,6 +18,39 @@ public:
     }
 };
 
+
+// OR
+
+class Logger {
+public:
+    /** Initialize your data structure here. */
+    unordered_set<string> S;
+    deque<pair<int, string>> Q;
+
+
+    Logger() {}
+
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    bool shouldPrintMessage(int timestamp, string message) {
+        while (!Q.empty() && Q.front().first <= timestamp) {
+            S.erase(Q.front().second);
+            Q.pop_front();
+        }
+
+        if (S.count(message)) return false;
+
+        Q.push_back({timestamp + 10, message});
+        S.insert(message);
+
+        return true;
+    }
+};
+
+// TC: O(1), amortized
+// SC: O(k) where k is the #messages printed in last 10 sec
+
 /**
  * Your Logger object will be instantiated and called as such:
  * Logger* obj = new Logger();
