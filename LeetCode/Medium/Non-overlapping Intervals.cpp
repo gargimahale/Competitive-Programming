c44750643c6e3eba0628811374fc69c9e0847298
gargimahale <gargimahale102@gmail.com>
2021-07-12 16:56:24 -0400

    Added LeetCode Files

--- LeetCode/Medium/Non-overlapping Intervals.cpp
+++ LeetCode/Medium/Non-overlapping Intervals.cpp
@@ -5,24 +5,24 @@ using namespace std;
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        
-        if (intervals.empty()){
-            return 0;
-        }
-        
-        sort(begin(intervals), end(intervals));
-        int cnt = 1, end = intervals[0][1];
-        
-        for (auto& p: intervals){
-            if (end > p[0]){
-                end = min(end, p[1]);
-            }
-            else{
-                end = p[1];
+        int n = intervals.size();
+        if (n <= 1) return 0;
+
+        sort(begin(intervals), end(intervals), [&](auto a, auto b) {
+            return a[1] < b[1];
+        });
+
+        int cnt = 0;
+        int first = intervals[0][1];
+
+        for (int i = 1; i < n; ++i) {
+            cout << intervals[i][0] << " " << intervals[i][1] << "\n";
+            if (intervals[i][0] < first) {
                 ++cnt;
+            } else {
+                first = intervals[i][1];
             }
         }
-        return intervals.size() - cnt;
+        return cnt;
     }
 };
-
