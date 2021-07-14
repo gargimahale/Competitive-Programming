6bc357bac42356cf01e622429c98af69fb55f3a7
gargimahale <gargimahale102@gmail.com>
2021-07-14 13:00:13 -0400

    Added LeetCode Files

---
+++ LeetCode/Medium/Unique Paths II.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+
+        if (m == 0) return 0;
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1));
+
+        dp[0][1] = 1;
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (!obstacleGrid[i - 1][j - 1]) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
