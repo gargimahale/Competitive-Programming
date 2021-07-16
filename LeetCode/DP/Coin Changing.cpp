bf324222d3aa5c7727c5a9a048c3fd51f01f142f
gargimahale <gargimahale102@gmail.com>
2021-07-16 19:36:50 -0400

    Added LeetCode Files

--- LeetCode/DP/Coin Changing.cpp
+++ LeetCode/DP/Coin Changing.cpp
@@ -5,17 +5,17 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
 
-        vector<int> dp (amount + 1, INT_MAX);
-
+        vector<int> dp (amount + 1, amount + 1);
         sort(begin(coins), end(coins));
+
         dp[0] = 0;
         for (int i = 1; i <= amount; ++i) {
-
             for (int j = 0; j < coins.size(); ++j) {
 
                 if (i >= coins[j]) {
                     dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
                 }
+
                 else {
                     // if coin is much larger than the curr amount
                     break;
---
+++ LeetCode/DP/Matrix Chain Multiplication.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int matrixMultiplyCost(const vector<vector<int>>& arr) {
+
+    }
+};
+
+
+int main(void) {
+    Solution sol;
+    vector<vector<int>> arr = {{2, 3} {3, 6}, {6, 4}, {4, 5}};
+    cout << sol.matrixMultiplyCost(arr);
+}
