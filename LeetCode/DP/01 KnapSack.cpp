b250b718850b6533d4a272146b82dc3f6320912d
gargimahale <gargimahale102@gmail.com>
2021-07-16 18:31:42 -0400

    Added LeetCode Files

--- LeetCode/DP/01 KnapSack.cpp
+++ LeetCode/DP/01 KnapSack.cpp
@@ -12,6 +12,7 @@ public:
 
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= cap; ++j) {
+
                 // when the cap is less than the cap of the item
                 if (j < wt[i]) {
                     // Take the max Value found so far by not considering the current item
