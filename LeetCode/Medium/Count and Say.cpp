4f133b13a356025e55ccbab7cb8678abd8ab7ebc
gargimahale <gargimahale102@gmail.com>
2021-07-21 18:32:08 -0400

    Added LeetCode Files

--- LeetCode/Medium/Count and Say.cpp
+++ LeetCode/Medium/Count and Say.cpp
@@ -1,30 +1,27 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 class Solution {
-public:
-    string countAndSay(int n) {
-        string res;
-        if (n == 0){
-            return res;
-        }
-        
-        res += "1";
-        while(--n){
-            string temp = "";
-            
-            for (int i=0; i<res.size(); ++i){
-                int count = 1;
-                while((i+1 < res.size()) && (res[i] == res[i+1])){
-                    ++count, ++i;
-                }
-                temp += to_string(count) + res[i];
+    string helperCountAndSay(string& res) const {
+        string temp;
+        int n = res.size();
+        for (int i = 0; i < n; ++i) {
+            int count = 1;
+            while (i + 1 < n && res[i] == res[i + 1]) {
+                ++i, ++count;
             }
-            
-            res = temp;
+            temp += to_string(count) + res[i];
         }
-        
+        return temp;
+    }
+public:
+    string countAndSay(int n) const {
+        if (n == 0) return "";
+
+        string res = "1";
+        for (int i = 1; i < n; ++i) {
+            res = helperCountAndSay(res);
+        }
         return res;
     }
 };
