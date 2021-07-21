66ba12ef965bf7489f80d0ffde6423049dbb8d4e
gargimahale <gargimahale102@gmail.com>
2021-07-21 17:06:24 -0400

    Added LeetCode Files

--- LeetCode/Easy/Excel Sheet Column Number.cpp
+++ LeetCode/Easy/Excel Sheet Column Number.cpp
@@ -7,8 +7,7 @@ public:
     int titleToNumber(string s) {
         int ans = 0;
         for (int i = 0; i < s.size(); ++i) {
-            ans *= 26;
-            ans += (s[i] - 'A') + 1;
+            ans = ans * 26 + (s[i] - 'A') + 1;
         }
         return ans;
     }
