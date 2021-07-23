0376f1607b713209d3d9131ecdcda46ac13efcb5
gargimahale <gargimahale102@gmail.com>
2021-07-23 09:56:31 -0400

    Added EPI Files

---
+++ EPI/templates.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Templates are expanded at compile time. They're like macros.
+The simple idea is to pass data type as a parameter so that
+we don’t need to write the same code for different data types
+ */
+
+template <typename T>
+T myMax(T x, T y) {
+    return x > y ? x : y;
+}
+
+int main(void) {
+    cout << myMax(3, 4) << "\n";
+    cout << myMax('c', 'd') << "\n";
+}
