3e6da983366fd3d1976325635063ecc6def0d1f8
gargimahale <gargimahale102@gmail.com>
2021-07-23 17:06:20 -0400

    Added Leetcode Files

--- LeetCode/Medium/rotate_list.cpp
+++ LeetCode/Medium/rotate_list.cpp
@@ -9,19 +9,28 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-ListNode* RotateRight(ListNode* head, int k){
-    if(!head) return NULL;
+ListNode* RotateRight(ListNode* head, int k) {
+    if (!head) return NULL;
+
     ListNode* curr = head;
-    int size=1;
-    while(curr->next){
+    int size = 1;
+
+    while (curr->next) {
         size++;
         curr = curr->next;
     }
+
     curr->next = head;
+
     // for k>size
-    k = k%size;
-    while(--size>=k) curr = curr->next;
+    k = k % size;
+
+    while (--size >= k)
+        curr = curr->next;
+
     ListNode* first = curr->next;
     curr->next = NULL;
+
     return first;
 }
+
