be5e9dbaa6bd9362e39bdf80dca613ff15cad84a
gargimahale <gargimahale102@gmail.com>
2021-07-24 17:32:23 -0400

    Added Leetcode Files

--- LeetCode/Easy/Number of 1 Bits.cpp
+++ LeetCode/Easy/Number of 1 Bits.cpp
@@ -8,3 +8,24 @@ public:
         return bitset<32>(n).count();
     }
 };
+
+// OR
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while (n) {
+            n &= (n - 1);
+            ++count;
+        }
+        return count;
+    }
+};
+
+// OR
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        return __builtin_popcount(n);
+    }
+};
