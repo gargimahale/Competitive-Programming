2bd30f2f0a81b1570b630e6d68ae506f61cab3ea
gargimahale <gargimahale102@gmail.com>
2021-07-28 00:53:34 -0400

    Added Leetcode Files

--- LeetCode/Easy/Merge Strings Alternately.cpp
+++ LeetCode/Easy/Merge Strings Alternately.cpp
@@ -1,30 +1,33 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int i = 0, j = 0, cnt = 0;
-        string res;
-        for (int cnt = 0; i < word1.size() && j < word2.size(); ) {
-            if (cnt % 2 == 0) {
-                res += word1[i++];
-                ++cnt;
+        int n = word1.size(), m = word2.size();
+        int i = 0, j = 0;
+        int x = 0;
+        string ans;
+
+        while (i < n || j < m) {
+            if ((x == 1 && j < m) || (i == n)) {
+                ans += word2[j++];
             }
             else {
-                res += word2[j++];
-                ++cnt;
+                ans += word1[i++];
             }
+            x ^= 1;
         }
-
-        while (i < word1.size()) {
-            res += word1[i++];
-        }
-
-        while (j < word2.size()) {
-            res += word2[j++];
-        }
-        return res;
+        return ans;
     }
 };
+
+
+// OR
+string mergeAlternately(string w1, string w2) {
+    int i = 0, j = 0;
+    string res;
+    while (i < w1.size() && j < w2.size())
+        res += string() + w1[i++] + w2[j++];
+    return res + w1.substr(i) + w2.substr(j);
+}
