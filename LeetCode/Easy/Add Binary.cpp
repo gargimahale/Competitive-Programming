d64403b59679287055044d967bd2f6382333fdd2
gargimahale <gargimahale102@gmail.com>
2021-07-31 14:10:40 -0400

    Added Leetcode Files

--- LeetCode/Easy/Add Binary.cpp
+++ LeetCode/Easy/Add Binary.cpp
@@ -19,3 +19,20 @@ public:
         return res;
     }
 };
+
+// OR
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i = a.size() - 1, j = b.size() - 1, c = 0;
+        string res;
+        while (i >= 0 || j >= 0 || c == 1) {
+            c += i >= 0 ? (a[i--] - '0') : 0;
+            c += j >= 0 ? (b[j--] - '0') : 0;
+            res = char(c % 2 + '0') + res;
+            c /= 2;
+        }
+        return res;
+    }
+};
