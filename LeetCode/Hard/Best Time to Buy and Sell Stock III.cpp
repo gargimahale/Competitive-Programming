c55a58010c8014e72557547d1645f9bc149e39f4
gargimahale <gargimahale102@gmail.com>
2021-07-31 19:37:14 -0400

    Added Leetcode Files

---
+++ LeetCode/Hard/Best Time to Buy and Sell Stock III.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        const int n = prices.size();
+        if (n < 2) return 0;
+
+        vector<int> profit1(n, 0), profit2(n, 0);
+        int p1 = prices[0], p2 = prices[n - 1];
+
+        for (int i = 1; i < n; ++i) {
+            profit1[i] = max(profit1[i - 1], prices[i] - p1);
+            p1 = min(p1, prices[i]);
+
+            int j = n - i - 1;
+            profit2[j] = max(profit2[j + 1], p2 - prices[j]);
+            p2 = max(p2, prices[j]);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; ++i) {
+            ans = max(ans, profit1[i] + profit2[i]);
+        }
+        return ans;
+    }
+};
--- LeetCode/Hard/N-Queens.cpp
+++ LeetCode/Hard/N-Queens.cpp
@@ -1,2 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+
+class Solution {
+public:
+
+    vector<vector<string>> ans;
+
+    bool isSafe(const vector<string>& board, int row, int col) {
+        // column check
+        for (int i = 0; i < board.size(); ++i) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+
+        int x = row, y = col;
+
+        // left diagonal
+        while (x >= 0 && y >= 0) {
+            if (board[x][y] == 'Q') {
+                return false;
+            }
+            --x, --y;
+        }
+
+        // right diagonal
+        while (row >= 0 && col >= 0) {
+            if (board[row][col] == 'Q') {
+                return false;
+            }
+            --row, ++col;
+        }
+
+        // safe
+        return true;
+    }
+
+    void placeQueen(vector<string>& board, int row) {
+        if (row == board.size()) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < board.size(); ++col) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                placeQueen(board, row + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(const int& n) {
+        if (n == 0) return ans;
+        vector<string> board(n, string(n, '.'));
+        placeQueen(board, 0);
+        return ans;
+    }
+};
