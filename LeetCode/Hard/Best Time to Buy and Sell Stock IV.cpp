cd822deb2394d78da003abe35acf9898cc00f5bf
gargimahale <gargimahale102@gmail.com>
2021-07-31 18:23:08 -0400

    Added Leetcode Files

---
+++ LeetCode/Hard/Best Time to Buy and Sell Stock IV.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+
+        if (n < 2 || k == 0) return 0;
+
+        vector<vector<int>> dp(k + 1, vector<int> (n));
+
+        for (int i = 1; i < dp.size(); ++i) {
+            for (int j = 1; j < dp[0].size(); ++j) {
+                int maxVal = 0;
+                for (int m = 0; m < j; ++m) {
+                    maxVal = max(maxVal, prices[j] - prices[m] + dp[i - 1][m]);
+                }
+                dp[i][j] = max(maxVal, dp[i][j - 1]);
+            }
+        }
+
+        return dp[k][n - 1];
+    }
+};
+
+// TC: O(days^2 * k)
+// SC: O(days * k)
+
+
+// OR
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+
+        if (n < 2 || k == 0) return 0;
+
+        vector<vector<int>> dp(k + 1, vector<int> (n));
+
+        for (int i = 1; i < dp.size(); ++i) {
+            int maxDiff = -prices[0];
+            for (int j = 1; j < dp[0].size(); ++j) {
+                dp[i][j] = max(dp[i][j - 1], prices[j] + maxDiff);
+                maxDiff = max(maxDiff, dp[i - 1][j] - prices[j]);
+            }
+        }
+
+        return dp[k][n - 1];
+    }
+};
+
+
+// TC and SC: O(Days * k)
--- LeetCode/Medium/Jump_Game2.cpp
+++ LeetCode/Medium/Jump_Game2.cpp
@@ -9,6 +9,26 @@ public:
         if (n < 2) {
             return 0;
         }
+        vector<int> dp(n, INT_MAX - 1);
+        dp[n - 1] = 0;
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = i + 1; j <= i + nums[i] && j < n; ++j) {
+                dp[i] = min(dp[i], 1 + dp[j]);
+            }
+        }
+        return dp[0];
+    }
+};
+
+// TC: O(N^2), SC: O(N)
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 2) {
+            return 0;
+        }
 
         int jumps = 1, currEnd = nums[0], currFarthest = nums[0];
         for (int i = 1; i < n; ++i) {
