6023af4c1c1fb8ee7aa7aeb21d4085e9de0dc928
gargimahale <gargimahale102@gmail.com>
2021-08-02 00:22:39 -0400

    Added Leetcode Files

--- LeetCode/Medium/Combination Sum IV.cpp
+++ LeetCode/Medium/Combination Sum IV.cpp
@@ -4,13 +4,13 @@ using namespace std;
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<unsigned int> comb(target+1);
-        
+        vector<unsigned int> comb(target + 1);
+
         comb[0] = 1;
-        for (int i=1; i<comb.size(); ++i){
-            for (int j = 0; j<nums.size(); ++j){
-                if (i - nums[j] >= 0){
-                    comb[i] += comb[i-nums[j]];
+        for (int i = 1; i < comb.size(); ++i) {
+            for (int j = 0; j < nums.size(); ++j) {
+                if (i - nums[j] >= 0) {
+                    comb[i] += comb[i - nums[j]];
                 }
             }
         }
---
+++ LeetCode/Medium/Combination Sum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> res;
+
+    void countCombinations(vector<int>& candidates, int target, vector<int> combination, int start) {
+        if (target == 0) {
+            res.push_back(combination);
+            return;
+        }
+
+        for (int i = start; i < candidates.size() && target >= candidates[i]; ++i) {
+            combination.push_back(candidates[i]);
+            countCombinations(candidates, target - candidates[i], combination, i);
+            combination.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(begin(candidates), end(candidates));
+        countCombinations(candidates, target, {}, 0);
+        return res;
+    }
+};
