aa942fea8292b16fc7efe6182766e99c94bddc3d
gargimahale <gargimahale102@gmail.com>
2021-08-04 13:34:24 -0400

    Added Leetcode Files

--- LeetCode/Medium/Car Fleet.cpp
+++ LeetCode/Medium/Car Fleet.cpp
@@ -24,3 +24,36 @@ public:
 
 // TC: O(nlogn)
 // SC: O(N)
+
+// OR
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+
+        int n = position.size(), m = speed.size();
+
+        if (!n || !m || !target) return 0;
+        vector<pair<int, double>> log;
+
+        for (int i = 0; i < n; ++i) {
+            double time = (double)(target - position[i]) / speed[i];
+            log.push_back({position[i], time});
+        }
+
+        sort(begin(log), end(log));
+
+        int fleets = 0;
+        double curr = 0;
+
+        for (int i = n - 1; i >= 0; --i) {
+            if (log[i].second > curr) {
+                curr = log[i].second;
+                ++fleets;
+            }
+        }
+
+        return fleets;
+    }
+};
+
+// TC: O(NlogN), SC: O(N)
