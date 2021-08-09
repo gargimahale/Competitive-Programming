6612ffd0c7247aafe735ed04f24c2206a81cf30e
gargimahale <gargimahale102@gmail.com>
2021-08-09 13:10:43 -0400

    Added AOC Files

--- Advent of Code/2020/Day 1: Report Repair/a.cpp
+++ Advent of Code/2020/Day 1: Report Repair/a.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 // 2-Sum
 
-int main(){
+int main() {
     ifstream myfile("inp.txt");
     unordered_map<int, bool> U;
     int target = 2020;
     int input;
-    while(myfile >> input){
-        if (U.find(target-input) != U.end()){
-            cout << input << " " << target-input << "\n";
+    while (myfile >> input) {
+        if (U.find(target - input) != U.end()) {
+            cout << input << " " << target - input << "\n";
             break;
-        }else{
+        } else {
             U[input] = 1;
         }
     }
--- Advent of Code/2020/Day 1: Report Repair/b.cpp
+++ Advent of Code/2020/Day 1: Report Repair/b.cpp
@@ -3,21 +3,21 @@ using namespace std;
 
 // 3-Sum
 
-void threeSum(vector<int>& nums, int target){
+void threeSum(vector<int>& nums, int target) {
     int ans = 1;
     sort(begin(nums), end(nums));
-    for (int i=0; i<nums.size()-2; ++i){
-        if (i == 0 || nums[i] != nums[i-1]){
-            int j = i+1, k = nums.size()-1;
-            while(j < k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if (sum == target){
-                    ans = nums[i]*nums[j]*nums[k];
+    for (int i = 0; i < nums.size() - 2; ++i) {
+        if (i == 0 || nums[i] != nums[i - 1]) {
+            int j = i + 1, k = nums.size() - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    ans = nums[i] * nums[j] * nums[k];
                     cout << ans << "\n";
                     return;
-                }else if (sum > target){
+                } else if (sum > target) {
                     --k;
-                }else{
+                } else {
                     ++j;
                 }
             }
@@ -25,11 +25,11 @@ void threeSum(vector<int>& nums, int target){
     }
 }
 
-int main(){
+int main() {
     ifstream myfile("inp.txt");
     int input, target = 2020;
     vector<int> nums;
-    while(myfile >> input){
+    while (myfile >> input) {
         nums.push_back(input);
     }
     threeSum(nums, target);
