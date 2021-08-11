ff8813fcf914e912205369674402fbe842dcd70e
gargimahale <gargimahale102@gmail.com>
2021-08-11 14:26:27 -0400

    Added Leetcode Files

--- LeetCode/Hard/longest_consecutive_seq.cpp
+++ LeetCode/Hard/longest_consecutive_seq.cpp
@@ -7,13 +7,13 @@ public:
     int longestConsecutive(vector<int>& nums) {
         unordered_set<int> S(nums.begin(), nums.end());
         int best = 0;
-        for (int x: nums){
-            if (S.find(x-1) == S.end()){
-                int y = x+1;
-                while (S.find(y)!=S.end()){
+        for (int x : nums) {
+            if (S.find(x - 1) == S.end()) {
+                int y = x + 1;
+                while (S.find(y) != S.end()) {
                     y += 1;
                 }
-                best = max(best, y-x);
+                best = max(best, y - x);
             }
         }
         return best;
--- LeetCode/Hard/median_of_two_sorted_arrays.cpp
+++ LeetCode/Hard/median_of_two_sorted_arrays.cpp
@@ -7,38 +7,38 @@ using namespace std;
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() > nums2.size()){
+        if (nums1.size() > nums2.size()) {
             return findMedianSortedArrays(nums2, nums1);
         }
 
         int x = nums1.size();
-        int y= nums2.size();
+        int y = nums2.size();
 
         int low = 0, high = x;
-        while(low <= high){
-            int partitionX = low+(high-low)/2;
-            int partitionY = (x+y+1)/2 - partitionX;
+        while (low <= high) {
+            int partitionX = low + (high - low) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
 
-            double maxLeftX = partitionX == 0 ? INT_MIN: nums1[partitionX-1];
-            double minRightX = partitionX == x ? INT_MAX: nums1[partitionX];
+            double maxLeftX = partitionX == 0 ? INT_MIN : nums1[partitionX - 1];
+            double minRightX = partitionX == x ? INT_MAX : nums1[partitionX];
 
-            double maxLeftY = partitionY == 0 ? INT_MIN: nums2[partitionY-1];
-            double minRightY = partitionY == y ? INT_MAX: nums2[partitionY];
+            double maxLeftY = partitionY == 0 ? INT_MIN : nums2[partitionY - 1];
+            double minRightY = partitionY == y ? INT_MAX : nums2[partitionY];
 
-            if (maxLeftX <= minRightY && maxLeftY <= minRightX){
-                if ((x+y)%2 == 0){
-                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY))/2.0;
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((x + y) % 2 == 0) {
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0;
                 }
-                else{
+                else {
                     return max(maxLeftX, maxLeftY);
                 }
             }
 
-            if (maxLeftX > minRightY){
-                high = partitionX-1;
+            if (maxLeftX > minRightY) {
+                high = partitionX - 1;
             }
-            else{
-                low = partitionX+1;
+            else {
+                low = partitionX + 1;
             }
         }
         return -1;
