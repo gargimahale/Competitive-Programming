f4ff08ebd6a236b2fcaf764d8ab52f0d4cecd873
gargimahale <gargimahale102@gmail.com>
2021-08-12 18:40:24 -0400

    Added Leetcode Files

--- LeetCode/Medium/Find Peak Element.cpp
+++ LeetCode/Medium/Find Peak Element.cpp
@@ -7,7 +7,6 @@ public:
         int left = 0, right = nums.size() - 1;
 
         while (left < right) {
-
             int mid = left + (right - left) / 2;
 
             if (nums[mid] < nums[mid + 1]) {
