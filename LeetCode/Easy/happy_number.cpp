7b618619d4930b428a4e348745935f8295000694
gargimahale <gargimahale102@gmail.com>
2021-08-13 21:35:08 -0400

    Added Leetcode Files

--- LeetCode/Easy/happy_number.cpp
+++ LeetCode/Easy/happy_number.cpp
@@ -28,3 +28,31 @@ public:
         return 0;
     }
 };
+
+// OR
+
+class Solution {
+    int findSquare(int& n) {
+        long ans = 0;
+        while (n > 0) {
+            ans += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return ans;
+    }
+public:
+    bool isHappy(int& n) {
+        unordered_set<int> seen;
+        seen.insert(n);
+        while (n > 1) {
+            n = findSquare(n);
+            if (seen.count(n) == 0) {
+                seen.insert(n);
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
