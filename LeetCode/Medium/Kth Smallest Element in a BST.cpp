b956df05f7a50e9c72bd14823493d16f42cd4b74
gargimahale <gargimahale102@gmail.com>
2021-08-27 20:01:46 -0400

    Updated LeetCode files

--- LeetCode/Medium/Kth Smallest Element in a BST.cpp
+++ LeetCode/Medium/Kth Smallest Element in a BST.cpp
@@ -1,36 +1,34 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
 class Solution {
 public:
-
-    void findKthSmallest(TreeNode* root, int k, vector<int>* kth) {
-        if (!root) return;
-
-        if (root && kth->size() < k) {
-            findKthSmallest(root->left, k, kth);
-            if (kth->size() < k) {
-                kth->push_back(root->val);
-                findKthSmallest(root->right, k, kth);
-            }
+    
+    void findKthSmallest(TreeNode* root, int& k, int& kth){
+        if (!root){
+            return;
         }
+        findKthSmallest(root->left, k, kth);
+        if (k > 0){
+            --k;
+            kth = root->val;
+        }
+        findKthSmallest(root->right, k, kth);
     }
-
+    
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> kth;
-        findKthSmallest(root, k, &kth);
-        return kth.back();
+        int kth = 0;
+        findKthSmallest(root, k, kth);
+        return kth;
     }
 };
