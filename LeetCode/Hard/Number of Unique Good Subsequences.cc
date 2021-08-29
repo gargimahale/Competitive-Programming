88bf05161299ac138d8056b541d4993231cd992b
gargimahale <gargimahale102@gmail.com>
2021-08-29 00:29:18 -0400

    Updated LeetCode files

---
+++ LeetCode/Hard/Number of Unique Good Subsequences.cc
@@ -0,0 +1,16 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfUniqueGoodSubsequences(string binary) {
+        int mod = 1e9+7, dp[2] = {0, 0};
+        
+        for (char& ch: binary){
+            dp[ch-'0'] = (dp[0] + dp[1] + ch -'0') % mod;
+        }
+        
+        return dp[0] + dp[1] + ((binary.find("0") != string::npos)) % mod;
+    }
+};
