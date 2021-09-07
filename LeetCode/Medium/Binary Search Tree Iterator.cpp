3c6a7f21b35f1cf707adbdc6b5e5b244b80f7f7e
gargimahale <gargimahale102@gmail.com>
2021-09-07 12:54:43 -0400

    Updated Files

--- LeetCode/Medium/Binary Search Tree Iterator.cpp
+++ LeetCode/Medium/Binary Search Tree Iterator.cpp
@@ -1,17 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+struct TreeNode{
+    int val;
+    TreeNode* left, *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 class BSTIterator {
 
     void dfs(TreeNode* root) {
@@ -36,6 +32,37 @@ public:
 
     bool hasNext() {
         return i < arr.size();
+    }
+};
+
+class BSTIterator {
+public:
+    
+    deque<int> Q;
+    void dfs(TreeNode* root){
+        if (root){
+            dfs(root->left);
+            Q.push_back(root->val);
+            dfs(root->right);
+        }
+    }
+    
+    
+    BSTIterator(TreeNode* root) {
+        dfs(root);
+    }
+    
+    int next() {
+        if (!Q.empty()){
+            int ele = Q.front();
+            Q.pop_front();
+            return ele;
+        }
+        return -1;
+    }
+    
+    bool hasNext() {
+        return !Q.empty();
     }
 };
 
