894070b34e49e8963e3e0f8c6031588cbb3450b8
gargimahale <gargimahale102@gmail.com>
2021-09-11 16:30:54 -0400

    Updated Files

--- LeetCode/Easy/Design HashMap.cpp
+++ LeetCode/Easy/Design HashMap.cpp
@@ -34,3 +34,54 @@ public:
  * int param_2 = obj->get(key);
  * obj->remove(key);
  */
+
+
+class MyHashMap {
+public:
+    /** Initialize your data structure here. */
+    vector<vector<pair<int, int>>> map;
+    const int size = 10000;
+    MyHashMap() {
+        map.resize(size);
+    }
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        int index = key % size;
+        vector<pair<int, int>>& row = map[index];
+        
+        for (auto it = row.begin(); it != row.end(); ++it){
+            if (it->first == key){
+                it->second = value;
+                return;
+            }
+        }
+        row.push_back({key, value});
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        int index = key % size;
+        vector<pair<int, int>>& row = map[index];
+        
+        for (auto it = row.begin(); it != row.end(); ++it){
+            if (it->first == key){
+                return it->second;
+            }
+        }
+        return -1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        int index = key % size;
+        vector<pair<int, int>>& row = map[index];
+        
+        for (auto it = row.begin(); it != row.end(); ++it){
+            if (it->first == key){
+                row.erase(it);
+                return;
+            }
+        }
+    }
+};
