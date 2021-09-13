47668082d55d7f5b06949d61102b7c3de9652eda
gargimahale <gargimahale102@gmail.com>
2021-09-13 01:44:32 -0400

    Updated Files

---
+++ LeetCode/Medium/Longest Substring with At Most K Distinct Characters .cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        vector<int> cnt(128);
+        int num = 0, i = 0, res = 0;
+        
+        for (int j = 0; j<s.size(); ++j){
+            if (cnt[s[j]]++ == 0) ++num;
+            
+            if (num > k){
+                while(--cnt[s[i++]] > 0);
+                --num;
+            }
+            res = max(res, j-i+1);
+        }
+        
+        return res;
+    }
+};
