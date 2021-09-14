cf562d1f5814d06d6641b83a414e29b8e38acd1b
gargimahale <gargimahale102@gmail.com>
2021-09-14 13:13:06 -0400

    Updated Files

--- LeetCode/Medium/Letter Combinations of a Phone Number.cpp
+++ LeetCode/Medium/Letter Combinations of a Phone Number.cpp
@@ -1,7 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+
+class Solution_R {
+    void helperCombinations(string& s, string curr, int i){
+        if (i == s.size()){
+            res.push_back(curr);
+            return;
+        }
+        
+        int d = s[i]-'0';
+        for (char ch: keyPad[d]){
+            helperCombinations(s, curr+ch, i+1);
+        }
+    }
+public:
+    vector<string> res;
+    const vector<string> keyPad = {"", "", "abc", "def", "ghi", "jkl", "mno",
+                                  "pqrs", "tuv", "wxyz"};
+    
+    vector<string> letterCombinations(string digits) {
+        if (!digits.size()){
+            return res;
+        }
+        string builder;
+        helperCombinations(digits, builder, 0);
+        return res;
+    }
+};
+
+class Solution_I {
 public:
     const vector<string> pad = {
         "", "", "abc", "def", "ghi", "jkl",
