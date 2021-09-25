22dec7c5855d8da564e9d2f56082060aa17590e9
gargimahale <gargimahale102@gmail.com>
2021-09-25 15:50:04 -0400

    Updated Files

--- LeetCode/Easy/Convert Sorted Array to Binary Search Tree.cpp
+++ LeetCode/Easy/Convert Sorted Array to Binary Search Tree.cpp
@@ -2,36 +2,33 @@
 using namespace std;
 
 // TC: O(n), SC: O(n)
+struct TreeNode{
+    int val;
+    TreeNode* left, *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
+    TreeNode* helperToBST(vector<int>& nums, int s, int e){
+        if (s > e)
+            return nullptr;
+        else{
+            int mid = (s + e + 1)/2;
+            TreeNode* newHead = new TreeNode(nums[mid]);
+            newHead->left = helperToBST(nums, s, mid-1);
+            newHead->right = helperToBST(nums, mid+1, e);
+            return newHead;
+        }
+    }
 public:
-    TreeNode *sortedArrayToBST(vector<int> &nums) {
-        if (nums.size() == 0)
-            return 0;
-
-        if (nums.size() == 1)
-            return new TreeNode(nums[0]);
-
-        int mid = nums.size() / 2;
-        TreeNode *root = new TreeNode(nums[mid]);
-
-        vector<int> lft(nums.begin(), nums.begin() + mid);
-        vector<int> rght(nums.begin() + mid + 1, nums.end());
-
-        root->left = sortedArrayToBST(lft);
-        root->right = sortedArrayToBST(rght);
-
-        return root;
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0){
+            return NULL;
+        }
+        
+        return helperToBST(nums, 0, n-1);
     }
 };
