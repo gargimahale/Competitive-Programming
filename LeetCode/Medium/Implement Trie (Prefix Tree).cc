1278324942a8dea0987492feae10bdb174456f6a
gargimahale <gargimahale102@gmail.com>
2021-09-26 18:35:36 -0400

    Updated Files

--- LeetCode/Medium/Implement Trie (Prefix Tree).cc
+++ LeetCode/Medium/Implement Trie (Prefix Tree).cc
@@ -60,10 +60,3 @@ int main(void){
     cout << obj->search("ape") << "\n";
     cout << obj->startsWith("ap") << "\n";
 }
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie* obj = new Trie();
- * obj->insert(word);
- * bool param_2 = obj->search(word);
- * bool param_3 = obj->startsWith(prefix);
- */
