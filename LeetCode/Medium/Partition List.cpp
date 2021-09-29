4a515119745de1c18158ee001253a7685fba07de
gargimahale <gargimahale102@gmail.com>
2021-09-29 10:32:11 -0400

    Updated Files

--- LeetCode/Medium/Partition List.cpp
+++ LeetCode/Medium/Partition List.cpp
@@ -1,30 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+class ListNode{
+public:
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
         ListNode left(0), right(0);
-        ListNode *l = &left, *r = &right;
-
-        while (head) {
-            ListNode* & ref = head->val < x ? l : r;
-            ref->next = head;
-            ref = ref->next;
+        ListNode* l = &left, *r = &right;
+        
+        while(head){
+            if (head->val < x){
+                l->next = head;
+                l = head;
+            }
+            else{
+                r->next = head;
+                r = head;
+            }
             head = head->next;
         }
-
+        
         l->next = right.next;
         r->next = NULL;
         return left.next;
--- LeetCode/Medium/Path Sum II.cpp
+++ LeetCode/Medium/Path Sum II.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+class TreeNode{
+public:
+    int val;
+    TreeNode* left, *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
     void findPaths(TreeNode *root, vector<vector<int>> &paths, int sum, vector<int> &path)
