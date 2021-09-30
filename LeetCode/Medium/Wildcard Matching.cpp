66f61d8c11e08d08ea72e6856e94083d8c3c8981
gargimahale <gargimahale102@gmail.com>
2021-09-29 21:54:18 -0400

    Updated Files

--- LeetCode/Medium/Wildcard Matching.cpp
+++ LeetCode/Medium/Wildcard Matching.cpp
@@ -4,18 +4,16 @@ using namespace std;
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int len1 = s.size(), len2 = p.size();
+        int n = s.size(), m = p.size();
         
-        vector<vector<bool>> dp(len1+1, vector<bool>(len2+1, false));
+        vector<vector<bool>> dp(n+1, vector<bool> (m+1));
         dp[0][0] = true;
         
+        for (int j = 1; j <= m && p[j-1] == '*'; ++j) 
+            dp[0][j] = true;
         
-        for (int j = 0; j < len2 && p[j] == '*'; ++j){
-            dp[0][j+1] = true;
-        }
-        
-        for (int i = 1; i<=len1; ++i){
-            for (int j = 1; j<=len2; ++j){
+        for (int i = 1; i <= n; ++i){
+            for (int j = 1; j <= m; ++j){
                 if (s[i-1] == p[j-1] || p[j-1] == '?'){
                     dp[i][j] = dp[i-1][j-1];
                 }
@@ -24,6 +22,6 @@ public:
                 }
             }
         }
-        return dp[len1][len2];
+        return dp[n][m];
     }
 };
