69f954a58095da37129b55de7b1f4fd3567874a5
gargimahale <gargimahale102@gmail.com>
2021-10-11 19:33:13 -0400

    Updated LeetCode Files

--- LeetCode/Medium/Linked List Cycle.cpp
+++ LeetCode/Medium/Linked List Cycle.cpp
@@ -1,25 +1,20 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+class ListNode{
+public:
+    int val;
+    ListNode* next;
+    ListNode(int x): val(x), next(nullptr) {}
+};
 
-// Floyds Cycle detection alsogrithm
-
+// Floyds Cycle detection algorithm
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
         if (!head) {
             return false;
         }
-
         ListNode* slow = head, *fast = head;
         while (fast && fast->next) {
             fast = fast->next->next;
