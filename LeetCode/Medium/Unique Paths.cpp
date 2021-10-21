ddb6a70442799950466e8e3d06872906c6b61713
gargimahale <gargimahale102@gmail.com>
2021-10-21 17:53:03 -0400

    Updated Leetcode Files

--- LeetCode/Medium/Unique Paths.cpp
+++ LeetCode/Medium/Unique Paths.cpp
@@ -19,6 +19,21 @@ public:
         return dp[m-1][n-1]; 
     }
 };
+// TC: O(M*N), SC: O(N*M)
 
-// TC: O(M*N)
-// SC: O(N*M)
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> prev(n, 1), curr(n, 1);
+        
+        for (int i = 1; i < m; ++i){
+            for (int j = 1; j < n; ++j){
+                curr[j] = prev[j] + curr[j-1];
+            }
+            swap(prev, curr);
+        }
+        return prev[n-1];
+    }
+};
+// TC: O(M*N), SC: O(N)
