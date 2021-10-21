7e01a710c70b7fe9491b254d6838e16a31e01a4b
gargimahale <gargimahale102@gmail.com>
2021-10-21 18:18:28 -0400

    Updated Leetcode Files

--- LeetCode/Medium/Jump_Game.cpp
+++ LeetCode/Medium/Jump_Game.cpp
@@ -18,3 +18,22 @@ public:
         return true;
     }
 };
+
+// OR
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size(), maxIdxReach = nums[0], limit = nums[0];
+        if (n < 2){
+            return true;
+        }
+        for (int i = 0; i < n; ++i){
+            if (i > maxIdxReach){
+                return false;
+            }
+            maxIdxReach = max(maxIdxReach, i+nums[i]);
+        }
+        return true;
+    }
+};
