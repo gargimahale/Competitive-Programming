6534da0b7d70b090d6ae80f3866bd2de40ff6280
gargimahale <gargimahale102@gmail.com>
2021-10-30 21:12:16 -0400

    Updated Advent-Of-Code 2020 Files

--- Advent of Code/2015/Day 2 - I Was Told There Would Be No Math/a.cpp
+++ Advent of Code/2015/Day 2 - I Was Told There Would Be No Math/a.cpp
@@ -1,7 +1,19 @@
-#include <bit/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-public:
-	
-};
+int main(void){
+	int a[3];
+	long long area = 0;
+
+	FILE *fp = fopen("input.txt", "r+");
+
+	while(fscanf(fp, "%dx%dx%d", a, a+1, a+2) != -1){
+		sort(a, a+3);
+		// part 1
+		// area += 2 * (a[0] * a[1] + a[1] * a[2] + a[2] * a[0]) + a[0] * a[1];
+		
+		// part 2
+		area += (2 * (a[0] + a[1])) + (a[0] * a[1] * a[2]);
+	}
+	cout << area << "\n";
+}
---
+++ Advent of Code/2015/Day 3 - Perfectly Spherical Houses in a Vacuum/b.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	ifstream file("input.txt");
+	string text;
+	file >> text;
+
+	pair<int, int> pos = {0, 0};
+	set<pair<int, int>> mp;
+	mp.insert(pos);
+	
+	int count = 1;
+	for (int i = 0; i < text.size(); i += 2){
+		
+		switch(text[i]){
+			case '^':
+					pos.first--;
+					break;
+
+			case '>':
+					pos.second++;
+					break;
+
+			case 'v':
+					pos.first++;
+					break;
+
+			case '<':
+				pos.second--;
+				break;
+		}
+
+		if (mp.count(pos) == 0){
+			++count;
+			mp.insert(pos);
+		}
+	}
+
+	pos = {0, 0};
+	for (int i = 1; i < text.size(); i += 2){
+		
+		switch(text[i]){
+			case '^':
+					pos.first--;
+					break;
+
+			case '>':
+					pos.second++;
+					break;
+
+			case 'v':
+					pos.first++;
+					break;
+
+			case '<':
+				pos.second--;
+				break;
+		}
+
+		if (mp.count(pos) == 0){
+			++count;
+			mp.insert(pos);
+		}
+	}
+
+	cout << count << "\n"; 
+}
