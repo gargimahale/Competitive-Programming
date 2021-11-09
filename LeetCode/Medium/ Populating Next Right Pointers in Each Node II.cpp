665a1797ac4e19d3f118ce52dc74c60c69698ba9
gargimahale <gargimahale102@gmail.com>
2021-11-09 15:55:15 -0500

    Updated Leetcode Files

--- LeetCode/Medium/ Populating Next Right Pointers in Each Node II.cpp
+++ LeetCode/Medium/ Populating Next Right Pointers in Each Node II.cpp
@@ -1,23 +1,16 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-/*
-class Node {
+class Node{
 public:
     int val;
-    Node* left;
-    Node* right;
-    Node* next;
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+    Node* left, *right, *next;
+    Node(): val(0), left(NULL), right(NULL), next(NULL) {}
     Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
+    Node(int _val, Node* _left, Node* _right, Node* _next) : val(_val), left(_left), right(_right), next(_next) {}
 };
-*/
 
 // TC: O(n), SC: O(1)
-
 class Solution{
 public:
     Node *connect(Node *root){
