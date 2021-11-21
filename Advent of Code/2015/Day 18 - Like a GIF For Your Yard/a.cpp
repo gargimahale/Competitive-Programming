940732656faf05163a7734f115bd0d986830fd4c
gargimahale <gargimahale102@gmail.com>
2021-11-21 17:33:34 -0500

    Updated Leetcode Files

--- Advent of Code/2015/Day 18 - Like a GIF For Your Yard/a.cpp
+++ Advent of Code/2015/Day 18 - Like a GIF For Your Yard/a.cpp
@@ -34,8 +34,7 @@ void processFile(const char *filename, const int steps, const bool forceCorners 
             state[0][inputSize - 1] = true;
             state[inputSize - 1][0] = true;
             state[inputSize - 1][inputSize - 1] = true;
-        }
-    };
+        }    };
 
     forceCornersFunc();
 
