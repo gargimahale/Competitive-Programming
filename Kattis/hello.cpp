7a2402d68289e575131f10ad37bd071be69f9d33
gargimahale <gargimahale102@gmail.com>
2021-12-25 01:13:46 -0500

    Updated Leetcode Files

--- Kattis/hello.cpp
+++ Kattis/hello.cpp
@@ -1,11 +1,6 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-	int j = 3;
-	int k = 7;
-	j += k;
-	k = j*2;
-	cout << "Hello There" << "\n";
-	return 0;
+int main(void){
+	cout << "Hello World!" << "\n";
 }
