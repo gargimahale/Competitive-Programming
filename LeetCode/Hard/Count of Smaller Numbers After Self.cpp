4178af22533ddf5b95fc8f997d5286da02fd65f0
gargimahale <gargimahale102@gmail.com>
2022-02-28 12:35:37 -0500

    Updated Leetcode Files

--- LeetCode/Hard/Count of Smaller Numbers After Self.cpp
+++ LeetCode/Hard/Count of Smaller Numbers After Self.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 
 class Solution {
-public:
-    
+public:  
 #define iterator vector<vector<int>>::iterator
 
     void sort_count(iterator l, iterator r, vector<int>& count) {
