054273482f8166469e9128ecdaf31d5790e4d798
gargimahale <gargimahale102@gmail.com>
2022-04-13 00:20:39 -0400

    Updated LeetCode Files

--- LeetCode/Hard/Find Minimum in Rotated Sorted Array II.cpp
+++ LeetCode/Hard/Find Minimum in Rotated Sorted Array II.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 class Solution {
 public:
+
     int findMin(vector<int> &nums) {
         int l = 0, h = nums.size() - 1;
+        
         while (l < h) {
             int m = l + (h - l) / 2;
             if (nums[m] > nums[h])
