236a9bf63c82f421c041d93459b9688b4f83442b
gargimahale <gargimahale102@gmail.com>
2022-04-15 11:03:52 -0400

    Updated LeetCode Files

--- LeetCode/Hard/Course Schedule III.cpp
+++ LeetCode/Hard/Course Schedule III.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 class Solution {
 public:
+    
     int scheduleCourse(vector<vector<int>>& courses) {
         if (courses.size() == 0) return 0;
         sort(begin(courses), end(courses), [&](auto a, auto b) {
             return a[1] < b[1];
         });
+
         priority_queue<int> heap;
         int now = 0;
 
