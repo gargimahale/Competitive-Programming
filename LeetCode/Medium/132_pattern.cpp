9d583e99cf00a910e711a2db7cb5c32edd3c8750
gargimahale <gargimahale102@gmail.com>
2022-05-01 16:47:26 -0400

    Added leetcode files

--- LeetCode/Medium/132_pattern.cpp
+++ LeetCode/Medium/132_pattern.cpp
@@ -4,8 +4,10 @@ using namespace std;
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
+        
         int second = INT_MIN;
         stack<int> st;
+        
         for (int i=nums.size()-1;i>=0;--i){
             if (nums[i] < second) return true;
             else{
