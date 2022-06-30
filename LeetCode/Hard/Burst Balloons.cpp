55c55c0382cc0c7c28b6e2be36d93229713c488e
gargimahale <gargimahale102@gmail.com>
2022-06-30 00:31:03 -0400

    Updated leetcode files.

--- LeetCode/Hard/Burst Balloons.cpp
+++ LeetCode/Hard/Burst Balloons.cpp
@@ -5,10 +5,8 @@ class Solution {
 public:
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
-
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
-
         vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), 0));
         
         for (int len = 1; len <= n; ++len){
